abece068c4c58ee81a97e2e07ce80fd0c80a560b
huhao <huhao@dnspod.com>
2013-07-04 14:03:02 +0800

    yumen

--- python/batch_import_domains.py
+++ python/batch_import_domains.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
 
-import requests
+import urllib
+import httplib
+import json
 
 login_email = 'xxx@xxx.com'
 login_password = 'password'
+
+__api_host = 'dnsapi.cn'
+__headers = {"Content-type": "application/x-www-form-urlencoded",
+             "Accept": "text/json",
+             "User-Agent": "domain.importer/0.0.1 (huhao@dnspod.com)"}
+
+def invoke_api(op, data):
+    data = urllib.urlencode(data)
+    conn = httplib.HTTPSConnection(__api_host)
+    conn.request('POST', '/' + op, data, __headers)
+    result = conn.getresponse().read()
+    conn.close()
+    return json.loads(result)
+
+
 
 def create_domain(domain):
     data = {'domain': domain,
@@ -13,8 +30,7 @@ def create_domain(domain):
             'error_on_empty': 'no'
             }
 
-    r = requests.post('https://dnsapi.cn/Domain.Create', data=data)
-    r = r.json()
+    r = invoke_api('Domain.Create', data=data)
     print 'create domain', domain, r['status']['code'], r['status']['message']
     return 0 if 'domain' not in r else r['domain']['id']
 
@@ -35,8 +51,7 @@ def create_record(domain, domain_id, sub_domain, value):
             'error_on_empty': 'no'
             }
 
-    r = requests.post('https://dnsapi.cn/Record.Create', data=data)
-    r = r.json()
+    r = invoke_api('/Record.Create', data=data)
     print 'create record', domain, sub_domain, r['status']['code'], r['status']['message']
 
 if __name__ == '__main__':
