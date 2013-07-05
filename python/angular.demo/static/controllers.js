bedfb70ed7aa591313a855c549ecbaf0e48e7f87
huhao <huhao@dnspod.com>
2013-07-05 10:42:42 +0800

    small change

--- python/angular.demo/static/controllers.js
+++ python/angular.demo/static/controllers.js
@@ -28,6 +28,7 @@ function DemoCtrl($scope, $http, $cookies) {
     $scope.addHouse = function(){
         var name = prompt("请输入您的姓名", '');
         var text = prompt("请输入您现在要说的话", '');
+        name = name.trim && name.trim(); 
         name = encodeURIComponent(name);
         var house = {
             name: name,
