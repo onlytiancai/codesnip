1c7299bbba52f1c36fb7c46214007d9e070e7fdf
huhao <huhao@dnspod.com>
2013-09-12 11:37:44 +0800

    汉语转Sql的demo

--- compilers/4/chinese_sql.py
+++ compilers/4/chinese_sql.py
@@ -12,7 +12,7 @@ tokens = (
     'QUERYDATA'   # 所取出的数据
 )
 
-t_TIMERANGE = r'(\d月份)|(今天)|(昨天)|(本周)|(上周)'
+t_TIMERANGE = r'(\d\d?月份)|(今天)|(昨天)|(本周)|(上周)'
 t_CONDITION = r'新增'
 t_DATA = r'员工'
 t_QUERYDATA = r'(总数)|(列表)'
@@ -26,23 +26,25 @@ def t_error(t):
 
 lexer = lex.lex(reflags=re.UNICODE)
 
+engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
+metadata = sqlalchemy.MetaData()
+emps = sqlalchemy.Table('emps', metadata,
+                        sqlalchemy.Column('name', sqlalchemy.String),
+                        sqlalchemy.Column('created_on', sqlalchemy.Date),
+                        )
+metadata.create_all(engine)
+
 
 def p_expression_plus(p):
-    'expression : TIMERANGE CONDITION DATA QUERYDATA'
-
-    engine = sqlalchemy.create_engine('sqlite:///:memory:', echo=True)
-    metadata = sqlalchemy.MetaData()
-    emps = sqlalchemy.Table('emps', metadata,
-            sqlalchemy.Column('name', sqlalchemy.String),
-            sqlalchemy.Column('created_on', sqlalchemy.Date),
-            )
-    metadata.create_all(engine)
+    '''
+    expression : TIMERANGE CONDITION DATA QUERYDATA 
+    '''
 
     str_timerange, str_condition, str_table, str_querydata = p[1], p[2], p[3], p[4]
 
     if str_table == '员工':
         if str_querydata == '总数':
-            sql = sqlalchemy.select([sqlalchemy.func.count(emps.c.name)])
+            sql = sqlalchemy.select([sqlalchemy.func.count(emps.c.name)]).as_scalar()
         elif str_querydata == '列表':
             sql = sqlalchemy.select([emps])
 
@@ -51,7 +53,14 @@ def p_expression_plus(p):
 
     print sql
 
-parser = yacc.yacc()
+def p_error(p):
+    print "Syntax error:%s" % p
 
-data = '''2月份新增员工总数'''
-result = parser.parse(data)
+
+datas = '''
+12月份新增员工列表
+本周新增员工总数
+'''
+for data in datas.split(): 
+    parser = yacc.yacc()
+    parser.parse(data)
