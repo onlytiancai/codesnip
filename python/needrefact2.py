ea40c624d3e3f9849aafe628d2672fc3fa3564fa
huhao <huhao@dnspod.com>
2013-09-27 17:35:44 +0800

    change

--- python/needrefact2.py
+++ python/needrefact2.py
@@ -1,66 +1,68 @@
-# -*- coding: utf-8 -*-
-'''Please let code becomes much simpler and easier to maintain.
-'''
+    # -*- coding: utf-8 -*-
+    '''Please let code becomes much simpler and easier to maintain.
+    '''
 
 
-def process(pet, action, target):
-    '''
-    >>> process('dog', 'eat', 'bone')
-    ok
-    >>> process('dog', 'eat', 'ball')
-    faild
-    >>> process('dog', 'play', 'ball')
-    ok
-    >>> process('dog', 'play', 'bone')
-    ok
-    >>> process('dolphin', 'play', 'ball')
-    ok
-    >>> process('dolphin', 'play', 'bone')
-    faild
-    >>> process('dolphin', 'eat', 'bone')
-    faild
-    >>> process('cat', 'catch', 'mouse')
-    Traceback (most recent call last):
-        ...
-    Exception
-    '''
-    if pet == 'dog':
-        if action == 'eat':
-            if target == 'bone':
-                print 'ok'
-            elif target == 'ball':
-                print 'faild'
+    def process(pet, action, target):
+        '''
+        >>> process('dog', 'eat', 'bone')
+        ok
+        >>> process('dog', 'eat', 'ball')
+        faild
+        >>> process('dog', 'play', 'ball')
+        yes
+        >>> process('dog', 'play', 'bone')
+        ok
+        >>> process('dolphin', 'play', 'ball')
+        good
+        >>> process('dolphin', 'play', 'bone')
+        faild
+        >>> process('dolphin', 'eat', 'bone')
+        faild
+        >>> process('dog', 'play', 'mouse')
+        opps
+        >>> process('cat', 'catch', 'mouse')
+        Traceback (most recent call last):
+            ...
+        Exception
+        '''
+        if pet == 'dog':
+            if action == 'eat':
+                if target == 'bone':
+                    print 'ok'
+                elif target == 'ball':
+                    print 'faild'
+                else:
+                    raise Exception()
+            elif action == 'play':
+                if target == 'bone':
+                    print 'ok'
+                elif target == 'ball':
+                    print 'yes'
+                else:
+                    print 'opps' 
             else:
                 raise Exception()
-        elif action == 'play':
-            if target == 'bone':
-                print 'ok'
-            elif target == 'ball':
-                print 'ok'
+        elif pet == 'dolphin':
+            if action == 'eat':
+                if target == 'bone':
+                    print 'faild'
+                elif target == 'ball':
+                    print 'faild'
+                else:
+                    raise Exception()
+            elif action == 'play':
+                if target == 'bone':
+                    print 'faild'
+                elif target == 'ball':
+                    print 'good'
+                else:
+                    raise Exception()
             else:
                 raise Exception()
         else:
             raise Exception()
-    elif pet == 'dolphin':
-        if action == 'eat':
-            if target == 'bone':
-                print 'faild'
-            elif target == 'ball':
-                print 'faild'
-            else:
-                raise Exception()
-        elif action == 'play':
-            if target == 'bone':
-                print 'faild'
-            elif target == 'ball':
-                print 'ok'
-            else:
-                raise Exception()
-        else:
-            raise Exception()
-    else:
-        raise Exception()
 
-if __name__ == '__main__':
-    import doctest
-    doctest.testmod()
+    if __name__ == '__main__':
+        import doctest
+        doctest.testmod()
