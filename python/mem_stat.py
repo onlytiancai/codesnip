70e189fadabe64292f50ae7141d849b4370d4e24
huhao <huhao@dnspod.com>
2013-10-22 18:38:30 +0800

    定时记录内存占用top 10的进程

---
+++ python/mem_stat.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+'''
+crontab设置每隔10分钟记录一次内存耗费Top 10
+
+10 * * * * cd /root/src/down-monitor/monitorv2/stuff && python mem_stat.py >>/var/log/mem_stat.log
+'''
+
+import psutil
+from datetime import datetime
+
+print '*' * 20, datetime.now()
+
+processes = psutil.get_process_list()
+ps = [(p.get_memory_info().vms, ' '.join(p.cmdline)) 
+      for p in processes
+      if p.name]
+
+for p in sorted(ps,reverse=True)[:10]:
+    print p
