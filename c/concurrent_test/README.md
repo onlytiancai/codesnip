ffd39583517a26b8a4badf5517bb4459764223b1
haohu <haohu@dnspod.com>
2013-11-14 16:45:24 +0800

    重构代码，增加并行算法2

--- c/concurrent_test/README.md
+++ c/concurrent_test/README.md
@@ -4,29 +4,42 @@
 
 ```
 make  //编译
-./concurrent_test.o test  //单元测试
+make test  //单元测试
+make profiler //进行性能剖析
+
 ./concurrent_test.o sum 1000 //串行求和迭代1000次
 ./concurrent_test.o sum1 1000 //并行求和迭代1000次
-```
-
-结果我写的并行求和还没串行求和跑的快，真令人费解
+./concurrent_test.o sum2 1000 //并行求和迭代1000次
 
 ```
-$./concurrent_test.o test
-expected=50038765
-sum1 result=50038765, test Ok
 
-$ time ./concurrent_test.o sum 1000
-sum excute 1000.
+四核机器上执行结果如下
+```
+$ make test //单元测试，全部通过
+./concurrent_test.o test
+expected=49959687
+sum1 result=49959687, test Ok
+sum2 result=49959687, test Ok
 
-real    0m2.054s
-user    0m2.050s
+$ time ./concurrent_test.o sum 100    //串行求和：228毫秒
+sum excute 100.
+
+real    0m0.228s
+user    0m0.220s
 sys 0m0.000s
 
-$ time ./concurrent_test.o sum1 1000
-sum1 excute 1000.
+$ time ./concurrent_test.o sum1 100   //并行求和版本1：389毫秒
+sum1 excute 100.
 
-real    0m4.002s
-user    0m14.360s
-sys 0m0.060s
+real    0m0.389s
+user    0m1.350s
+sys 0m0.000s
+
+$ time ./concurrent_test.o sum2 100   //并行求和版本2: 86毫秒
+sum2 excute 100.
+
+real    0m0.086s
+user    0m0.240s
+sys 0m0.010s
+
 ```
--- c/concurrent_test/concurrent_test.c
+++ c/concurrent_test/concurrent_test.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include <pthread.h>
 
 #define N 1000000 //定义求和数组的长度
 #define cpu_count 4 //定义CPU个数
 
 static int array[N]; //被求和的数组
-static int sum1_results[cpu_count]; //保存sum1并行求和算法的临时结果
+static int partial_results[cpu_count]; //保存各线程的分部结果
 
 //随机初始化一个被求和数组
 void init_array() {
@@ -19,39 +20,31 @@ void init_array() {
     }
 }
 
-//sum1算法中每个线程求一部分数组元素的和
-void sum1_partial_sum(int* pcpu_index){
-    int cpu_index = *pcpu_index;
-
-    int len = (N - 1) / cpu_count + 1;
-    int begin, end;
-    begin = len * cpu_index;
-    end = begin + len;
-    if (end > N) { end = N; }
-
+//重置各线程的分部结果
+void reset_partial_results(){
     int i;
-    for (i = begin; i < end; i++) {
-        sum1_results[cpu_index] += array[i];
+    for (i = 0; i < cpu_count; i++) {
+        partial_results[i] = 0;
     }
 }
 
-//sum1算法的合并各线程结果
-int sum1_merge_partial_result(){
+//合并各线程结果
+int merge_partial_result(){
     int i, result = 0;
     for (i = 0; i < cpu_count; i++) {
-        result += sum1_results[i];
+        result += partial_results[i];
     }
     return result;
 }
 
-//sum1算法，起cpu_count个线程，每个线程求数组的一部分的和，最后汇总各线程结果
-int sum1(){
+//多线程执行一个函数，传入的函数指针需指向一个**接受一个int指针参数并返回void的函数**, 参数将会是cpu_index
+void parallel_run(void (*proc)(int*)){
     pthread_t tid[cpu_count];
     int cpu_index, ret, indexes[cpu_count];
     for (cpu_index = 0; cpu_index < cpu_count; cpu_index++) {
         indexes[cpu_index] = cpu_index;
 
-        ret=pthread_create(&tid[cpu_index], NULL, (void *)sum1_partial_sum, &indexes[cpu_index]);
+        ret=pthread_create(&tid[cpu_index], NULL, (void *)proc, &indexes[cpu_index]);
         if(ret!=0){
             printf ("Create pthread error!\n");
             exit (1);
@@ -63,7 +56,6 @@ int sum1(){
           pthread_join(tid[cpu_index], NULL);
     }
 
-    return sum1_merge_partial_result();
 }
 
 //串行求和算法，当作单元测试中的期望值
@@ -75,8 +67,59 @@ int sum() {
     return result;
 }
 
+//并行求和算法1中的线程执行函数
+void sum1_proc(int* pcpu_index){
+    int cpu_index = *pcpu_index;
+
+    //得到要处理的数据边界
+    int len = (N - 1) / cpu_count + 1;
+    int begin, end;
+    begin = len * cpu_index;
+    end = begin + len;
+    if (end > N) { end = N; }
+
+    int i;
+    for (i = begin; i < end; i++) {
+        //多个线程间频繁交替写全局变量partial_results，造成cacheline失效
+        partial_results[cpu_index] += array[i];
+    }
+}
+
+//并行求和版本1
+int sum1(){
+    reset_partial_results();
+    parallel_run(&sum1_proc); 
+    return merge_partial_result();
+}
+
+//并行求和版本2中的线程执行函数
+void sum2_proc(int* pcpu_index){
+    int cpu_index = *pcpu_index;
+
+    int len = (N - 1) / cpu_count + 1;
+    int begin, end;
+    begin = len * cpu_index;
+    end = begin + len;
+    if (end > N) { end = N; }
+
+    int i, temp_result = 0;
+    for (i = begin; i < end; i++) {
+        temp_result += array[i];
+    }
+    //计算完毕后写一次全局变量partial_results，性能提高很多
+    partial_results[cpu_index] = temp_result;
+}
+
+//并行求和版本2
+int sum2(){
+    reset_partial_results();
+    parallel_run(&sum2_proc); 
+    return merge_partial_result();
+}
+
+
 //单元测试
-int unit_test(){
+void unit_test(){
     int expected = sum();
     int result = 0; 
 
@@ -84,6 +127,10 @@ int unit_test(){
 
     result = sum1(); 
     printf("sum1 result=%d, test %s\n", result, result == expected ? "Ok": "faild");
+
+
+    result = sum2(); 
+    printf("sum2 result=%d, test %s\n", result, result == expected ? "Ok": "faild");
 }
 
 int main(int argc, const char *argv[]) {
@@ -111,6 +158,9 @@ int main(int argc, const char *argv[]) {
     }
     else if (strcmp(action, "sum1") == 0) {
         sumfunc = &sum1;
+    }
+    else if (strcmp(action, "sum2") == 0) {
+        sumfunc = &sum2;
     }else{
         sumfunc = &sum;
     }
