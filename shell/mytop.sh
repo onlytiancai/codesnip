c94af22ec4ef0a88409f82710efde879b8b89e46
huhao <huhao@dnspod.com>
2014-01-25 10:33:14 +0800

    x

--- shell/mytop.sh
+++ shell/mytop.sh
@@ -1,5 +1,5 @@
-API_KEY=$1
-COLLECTOR_IP=$2
+API_KEY=${API_KEY:="test_api_key"}
+COLLECTOR_IP=${COLLECTOR_IP:="127.0.0.1"}
 COLLECTOR_PORT="2003"
 
 IP_ADDRS=`ifconfig | grep 'inet addr' | grep -v '255.0.0.0' | cut -f2 -d':' | awk '{print $1}'`
@@ -17,7 +17,8 @@ function collect(){
     mem_use=`echo  "$info" | sed -n '4p' | awk '{print $5}' | sed -r 's/([0-9]+).*/\1/'`
     swap_use=`echo  "$info" | sed -n '5p' | awk '{print $4}' | sed -r 's/([0-9]+).*/\1/'`
 
-    echo "load=$load, tasks=$tasks, cpu_use=$cpu_use, mem_use=$mem_use, swap_use=$swap_use"
+    echo "collector_ip=$COLLECTOR_IP, collector_port=$COLLECTOR_PORT, api_key=$API_KEY"
+    echo "load=$load, tasks=$tasks, cpu_use=$cpu_use, mem_use=$mem_use, swap_use=$swap_use, "
     echo "$API_KEY/$HOSTNAME/$IP_ADDRS/load $load $time"  | nc $COLLECTOR_IP $COLLECTOR_PORT 
     echo "$API_KEY/$HOSTNAME/$IP_ADDRS/tasks $tasks $time" | nc $COLLECTOR_IP $COLLECTOR_PORT 
     echo "$API_KEY/$HOSTNAME/$IP_ADDRS/cpu_use $cpu_use $time" | nc $COLLECTOR_IP $COLLECTOR_PORT 
@@ -28,7 +29,7 @@ function collect(){
 
 while :
 do
+    echo `date`
     collect;
     sleep 60;
-    echo `date`
 done;
