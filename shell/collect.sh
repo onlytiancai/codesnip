4400ad082b43b6926aedf37feb225bd0a9780020
haohu <haohu@dnspod.com>
2014-01-27 15:59:54 +0800

    x

--- shell/collect.sh
+++ shell/collect.sh
@@ -28,7 +28,7 @@ collect() {
     mem_use=`echo  "$info" | sed -n "4p" | grep -Eo ",.* used" | grep -Eo "[0-9]+"`
     swap_use=`echo  "$info" | sed -n "5p" | grep -Eo ",.* used" | grep -Eo "[0-9]+"`
 
-    echo "load=$load, tasks=$tasks, cpu_use=$cpu_use, mem_use=$mem_use, swap_use=$swap_use"
+    echo "will send: load=$load, tasks=$tasks, cpu_use=$cpu_use, mem_use=$mem_use, swap_use=$swap_use"
 
     echo "$API_KEY/$HOSTNAME/$IP_ADDRS/load $load $time"  | nc $COLLECTOR_IP $COLLECTOR_PORT >/dev/null
     echo "$API_KEY/$HOSTNAME/$IP_ADDRS/tasks $tasks $time" | nc $COLLECTOR_IP $COLLECTOR_PORT >/dev/null
@@ -44,10 +44,25 @@ collect() {
         i=`expr $i + 1`
         rx_bytes=`ifconfig | grep "RX bytes" | sed -n "${i}p" | grep -Eo "RX bytes:[0-9]+" | grep -Eo "[0-9]+"`
         tx_bytes=`ifconfig | grep "TX bytes" | sed -n "${i}p" | grep -Eo "TX bytes:[0-9]+" | grep -Eo "[0-9]+"`
-        echo rx_bytes_$card=$rx_bytes, tx_bytes_$card=$tx_bytes
+        echo "will send:rx_bytes_$card=$rx_bytes, tx_bytes_$card=$tx_bytes"
         echo "$API_KEY/$HOSTNAME/$IP_ADDRS/rx_bytes_$card $rx_bytes $time" | nc $COLLECTOR_IP $COLLECTOR_PORT >/dev/null
         echo "$API_KEY/$HOSTNAME/$IP_ADDRS/tx_bytes_$card $tx_bytes $time" | nc $COLLECTOR_IP $COLLECTOR_PORT >/dev/null
     done;
+    echo
+
+    #df info
+    disks=`df -h | grep -Eo "^/dev/sda[0-9]" | sed -r "s/\//_/g"`
+    i=0
+    for disk in $disks;do
+        i=`expr $i + 1`
+        disk_use=`df -h | grep -E "^/dev/sda[0-9]" | sed -n "${i}p" | grep -Eo "[0-9]+%" | grep -Eo "[0-9]+"`
+        tx_bytes=`ifconfig | grep "TX bytes" | sed -n "${i}p" | grep -Eo "TX bytes:[0-9]+" | grep -Eo "[0-9]+"`
+        echo "will send:disk_use.$disk=$disk_use"
+        echo "$API_KEY/$HOSTNAME/$IP_ADDRS/disk_use.$disk $disk_use $time" | nc $COLLECTOR_IP $COLLECTOR_PORT >/dev/null
+    done;
+    echo
+
+
 
 }
 
