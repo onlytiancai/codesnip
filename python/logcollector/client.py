a52879516d723158ee8fd713c7ac817fd40c59b5
onlytiancai <onlytiancai@gmail.com>
2016-05-12 22:57:55 +0800

    log collector

--- python/logcollector/client.py
+++ python/logcollector/client.py
@@ -1,23 +1,30 @@
+import os
+import sys
+import time
 import logging, logging.handlers
 
 rootLogger = logging.getLogger('')
 rootLogger.setLevel(logging.DEBUG)
-socketHandler = logging.handlers.SocketHandler('localhost',
-                    logging.handlers.DEFAULT_TCP_LOGGING_PORT)
-# don't bother with a formatter, since a socket handler sends the event as
-# an unformatted pickle
+socketHandler = logging.handlers.SocketHandler('localhost', logging.handlers.DEFAULT_TCP_LOGGING_PORT)
 rootLogger.addHandler(socketHandler)
 
-# Now, we can log to the root logger, or any other logger. First the root...
-logging.info('Jackdaws love my big sphinx of quartz.')
+def tail_f(filename):
+    interval = 1.0
 
-# Now, define a couple of other loggers which might represent areas in your
-# application:
+    file = open(filename,'r')
+    st_results = os.stat(filename)
+    st_size = st_results[6]
+    file.seek(st_size)
 
-logger1 = logging.getLogger('myapp.area1')
-logger2 = logging.getLogger('myapp.area2')
+    while True:
+        where = file.tell()
+        line = file.readline()
+        if not line:
+            time.sleep(interval)
+            file.seek(where)
+        else:
+            yield line
 
-logger1.debug('Quick zephyrs blow, vexing daft Jim.')
-logger1.info('How quickly daft jumping zebras vex.')
-logger2.warning('Jail zesty vixen who grabbed pay from quack.')
-logger2.error('The five boxing wizards jump quickly.')
+for line in tail_f(sys.argv[1]):
+    print line
+    logging.info(line)
