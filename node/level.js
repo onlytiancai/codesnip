56ae6febb914fab8e0a005a7c1a0e2fa3f2594be
onlytiancai <onlytiancai@gmail.com>
2016-05-14 21:59:45 +0800

    test ok

--- node/level.js
+++ node/level.js
@@ -1,33 +1,76 @@
+/*
+== 介绍：
+
+使用leveldb来接受事件流，并进行实时计算。
+目前是只做根据event_name纬度进行1分钟力度的加总。
+
+== 测试： 
+
+    mocha level.js
+
+== todo:
+
+- 测试用例用的mapSeries来添加事件，如果用map，在_statEvent里取到的汇总值会为空
+- moments进行时间多次转换，有些浪费
+
+*/
 var levelup = require('levelup');
 var memdown = require('memdown');
+var moment = require('moment');
 var async = require('async');
-var assert = require('chai').assert;
+var assert = require('assert');
+var uuid = require('node-uuid');
 
 var db = levelup({ db: memdown });
 
-db.put('name', 'LevelUP', function (err) {
-  if (err) return console.log('Ooops!', err)
+function _statEvent(appid, time, data, callback) {
+    var time = moment(time).format('YYYYMMDDHHmm');
+    var key = ['stat-events', appid, data.event_name, time].join(':');
 
-  db.get('name', function (err, value) {
-    if (err) return console.log('Ooops!', err)
+    db.get(key, function (err, old_value) {
+        old_value = err ? {sum: 0} : JSON.parse(old_value); 
+        var value = {sum: old_value.sum + data.value};
 
-    console.log('name=' + value)
-  })
-})
+        console.log('statEvent:', key, value, old_value, data.value);
+        db.put(key, JSON.stringify(value), callback);
+    })
+}
+
 
 function addEvent(appid, time, data, callback) {
-    console.log('addEvent', arguments);
-    callback(null);
+    console.log('addEvent:', appid, time, data);
+
+    var event_id = uuid.v4();
+    var event_time = moment(time).format('YYYYMMDDHHmmss');
+    var key = ['events', appid, event_time , event_id].join(':'); 
+
+    _statEvent(appid, time, data, function(err){
+        if (err) return console.log('statEvent err')
+        db.put(key, JSON.stringify(data), callback);
+    });
 }
+
 
 function queryEvents(appid, event_name, callback) {
-    var ret = [
-        {time: new Date(2016, 4, 14, 17, 17, 0), value: 9}, 
-        {time: new Date(2016, 4, 14, 17, 18, 0), value: 2}, 
-    ];
-    callback(null, ret);
+    var ret = [];
+    var start = ['stat-events', appid, event_name, ''].join(':');
+    var end = ['stat-events', appid, event_name, '~'].join(':');
+
+    db.createReadStream({ start: start, end: end })
+    .on('data', function (data) {
+        console.log('stream-data:', data.key, '=', data.value)
+
+        var time = moment(data.key.split(':')[3], 'YYYYMMDDHHmm').toDate();
+        var value = JSON.parse(data.value);
+        ret.push({time: time, sum: value.sum});
+    })
+    .on('end', function () {
+        callback(null, ret);
+    });
+
 }
 
+// ============= unit test
 
 var test_data = [
     {time: new Date(2016, 4, 14, 17, 17, 0), data: {event_name: 'click', value: 3}},
@@ -40,24 +83,22 @@ var test_data = [
 var test_appid = 10000;
 
 describe('leveldb', function() {
-  describe('#hook-and-map-reduce', function () {
+  describe('#stat-events', function () {
     it('will ok', function (done) {
 
-        async.map(test_data, function(data, callback){
-
+        async.mapSeries(test_data, function(data, callback){
             addEvent(test_appid, data.time, data.data, callback)
-
         },function(){
 
             queryEvents(test_appid, 'click', function(err, data){
                 console.log(data);
-                assert.equal(2, data.length);
+                assert(2, data.length);
 
-                assert.equal(new Date(2016, 4, 14, 17, 17, 0).getTime(), data[0].time.getTime());
-                assert.equal(9, data[0].value);
+                assert(new Date(2016, 4, 14, 17, 17, 0).getTime(), data[0].time.getTime());
+                assert(9, data[0].sum);
 
-                assert.equal(new Date(2016, 4, 14, 17, 18, 0).getTime(), data[1].time.getTime());
-                assert.equal(2, data[1].value);
+                assert(new Date(2016, 4, 14, 17, 18, 0).getTime(), data[1].time.getTime());
+                assert(2, data[1].sum);
 
                 done(); 
             }); 
