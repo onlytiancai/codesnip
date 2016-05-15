072de43b604ce53040a4b2e127f6032925c7e248
onlytiancai <onlytiancai@gmail.com>
2016-05-15 13:12:33 +0800

    去掉throght的依赖

--- node/stream-group.js
+++ node/stream-group.js
@@ -13,22 +13,19 @@
  * */
 
 var from = require('from');
-var through = require('through');
 var assert = require('assert');
 
 function groupStream(stream, groupby, metric, callback) {
     var ret = {};
-    stream.pipe(through(function write(data) {
-            console.log('write:', data);
-            var group = data[groupby];
-            var value = ret[group] || 0;
-            ret[group] = value + data[metric];
-        },
-        function end () {
-            console.log('end:', ret);
-            callback(null, ret);
-        })
-    );
+    stream.on('data', function(data) {
+        console.log('write:', data);
+        var group = data[groupby];
+        var value = ret[group] || 0;
+        ret[group] = value + data[metric];
+     }).on('end', function () {
+        console.log('end:', ret);
+        callback(null, ret);
+     });
 }
 
 var test_data = [
