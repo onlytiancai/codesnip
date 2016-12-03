05247eae264df2756af5fc8571b02c50e768995f
onlytiancai <onlytiancai@gmail.com>
2016-12-03 17:25:56 +0800

    字体，颜色，加粗，斜体，下划线，边框颜色，边框宽度，垂直对齐，水平对齐，背景颜色都支持了

--- javascript/excel_show/static/main.js
+++ javascript/excel_show/static/main.js
@@ -62,19 +62,50 @@ function process_wb(workbook) {
     }
     console.table(mergeCells);
 
-    var colWidths = worksheet['!cols'].map(function(x) {return x.wpx});
+    var colWidths = worksheet['!cols'] ? worksheet['!cols'].map(function(x) {return x.wpx}) : undefined;
 
+    // http://stackoverflow.com/questions/6491463/accessing-nested-javascript-objects-with-string-key
+    var byString = function(o, s) {
+        s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+        s = s.replace(/^\./, '');           // strip a leading dot
+        var a = s.split('.');
+        for (var i = 0, n = a.length; i < n; ++i) {
+            var k = a[i];
+            if (k in o) {
+                o = o[k];
+            } else {
+                return;
+            }
+        }
+        return o;
+    }
 
     function myRenderer(instance, td, row, col, prop, value, cellProperties) {
         Handsontable.renderers.TextRenderer.apply(this, arguments);
         var cell_index = XLSX.utils.encode_cell({c:col, r:row});
         var cell = worksheet[cell_index];
         if (cell) {
-            if (cell.r && cell.r.substring(0,3) === '<t>') td.style.fontWeight = 'bold';
-            if (cell.s) {
-                td.style.color = cell.s.fgColor.rgb;
-                if (cell.s.bgColor.indexed === 64) td.style.background = 'yellow';
-            }
+            if (byString(cell, 's.font.color.rgb')) td.style.color = '#' + cell.s.font.color.rgb.substring(2, 8);
+            if (byString(cell, 's.font.bold')) td.style.fontWeight = cell.s.font.bold ? 'bold': 'normal';
+            if (byString(cell, 's.font.italic')) td.style.fontStyle = cell.s.font.italic ? 'italic' : 'normal';
+            if (byString(cell, 's.font.name')) td.style.fontFamily = cell.s.font.name;
+            if (byString(cell, 's.font.sz')) td.style.fontSize = cell.s.font.sz + 'pt';
+
+            if (byString(cell, 's.alignment.vertical')) td.style.verticalAlign = cell.s.alignment.vertical;
+            if (byString(cell, 's.alignment.horizontal')) td.style.textAlign = cell.s.alignment.horizontal;
+
+            if (byString(cell, 's.font.underline')) td.style.textDecoration = cell.s.font.underline ? 'underline' : 'none';
+            if (byString(cell, 's.fill.fgColor.rgb')) td.style.backgroundColor = '#' + cell.s.fill.fgColor.rgb.substring(2, 8);
+
+            if (byString(cell, 's.border.top.style')) td.style.borderTopWidth = cell.s.border.top.style == 'medium' ? '2px' : '1px';
+            if (byString(cell, 's.border.right.style')) td.style.borderRightWidth= cell.s.border.right.style == 'medium' ? '2px' : '1px';
+            if (byString(cell, 's.border.bottom.style')) td.style.borderBottomWidth = cell.s.border.bottom.style == 'medium' ? '2px' : '1px';
+            if (byString(cell, 's.border.left.style')) td.style.borderLeftWidth = cell.s.border.left.style == 'medium' ? '2px' : '1px';
+            if (byString(cell, 's.border.top.color.rgb')) td.style.borderTopColor = '#' + cell.s.border.top.color.rgb.substring(2, 8);
+            if (byString(cell, 's.border.right.color.rgb')) td.style.borderRightColor = '#' + cell.s.border.right.color.rgb.substring(2, 8);
+            if (byString(cell, 's.border.bottom.color.rgb')) td.style.borderBottomColor = '#' + cell.s.border.bottom.color.rgb.substring(2, 8);
+            if (byString(cell, 's.border.left.color.rgb')) td.style.borderLeftColor= '#' + cell.s.border.left.color.rgb.substring(2.8);
+
         }
     }
 
