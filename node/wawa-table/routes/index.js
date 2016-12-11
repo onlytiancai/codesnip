117525dc3d4a71bf26f103ce3504ea62f669f701
onlytiancai <onlytiancai@gmail.com>
2016-12-11 23:00:42 +0800

    qq login ok

--- node/wawa-table/routes/index.js
+++ node/wawa-table/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '蛙蛙表格', user: req.user ? req.user.nickname : '' });
 });
 
 module.exports = router;
