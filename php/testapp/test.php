3acfae8433900f54130719dd8284e93446ef03d7
onlytiancai <onlytiancai@gmail.com>
2016-12-11 20:10:46 +0800

    stuff

--- php/testapp/test.php
+++ php/testapp/test.php
@@ -1,8 +1,14 @@
 <?php
 require 'vendor/autoload.php';
 
+use Noodlehaus\Config;
+
+$conf = Config::load('config.ini');
+$logfile = $conf->get('app.logfile', 'app.log');
+
+echo sprintf("logfile = %s\n", $logfile);
+
 $log = new Monolog\Logger('name');
-$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+$log->pushHandler(new Monolog\Handler\StreamHandler($logfile, Monolog\Logger::WARNING));
 
 $log->addWarning('Foo');
-
