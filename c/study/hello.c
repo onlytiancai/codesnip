24cbe6388e46ee7f24f4735d155d7d9c3b5a040d
onlytiancai <onlytiancai@gmail.com>
2017-10-09 23:53:11 +0800

    code review memo

---
+++ c/study/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "hello.h"
+
+void hello(const char* name)
+{
+    printf("hello %s!\n", name);
+}
---
+++ c/study/hello.h
@@ -0,0 +1,4 @@
+#ifndef HELLO_
+#define HELLO_
+void hello(const char* name);
+#endif //HELLO_
