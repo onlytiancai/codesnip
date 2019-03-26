65824e4734790fbb6653631675e9ed127b530d4e
onlytiancai <onlytiancai@gmail.com>
2019-03-26 13:49:57 +0800

    x

--- go/hello.go
+++ go/hello.go
@@ -2,6 +2,25 @@ package main
 
 import "fmt"
 
+func aaa (s *string) {
+    s = nil
+}
+
+func bbb(s *string) {
+    x :=  "hello"
+    s = &x 
+}
+
 func main() {
-    fmt.Printf("hello, world\n")
+    var s *string
+
+    aaa(s) 
+    if (s == nil) {
+        fmt.Printf("s is nil\n")
+    } else {
+        fmt.Printf("%s\n", *s)
+    }
+
+    bbb(s)
+    fmt.Printf("%s\n", *s)
 }
