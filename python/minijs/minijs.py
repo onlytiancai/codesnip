168a21723187394df00b9eba525c2b758a2f4204
haohu <haohu@techaction.cn>
2019-04-28 14:04:54 +0800

    x

--- python/minijs/minijs.py
+++ python/minijs/minijs.py
@@ -9,15 +9,26 @@ rules = [
     ('-', r'\-'),
     ('*', r'\*'),
     ('/', r'\/'),
+    ('=', r'='),
+    (';', r';'),
     ('num', r'\d+'),
+    ('var', r'var'),
+    ('name', r'\w+'),
 ]
+
+
+def get_patten(x): return x[1]
 
 
 def parse_token(input):
     '根据正则规则做最长匹配，返回匹配结果及剩余字符'
-    matchs = map(lambda rule: (rule, re.match(rule[1], input)), rules)
-    matchs = filter(lambda x: x[1] is not None, matchs)
-    max_match = max(matchs, key=lambda x: x[1].end)
+    matchs = map(lambda rule: (rule, re.match(get_patten(rule), input)), rules)
+
+    matchs = list(filter(lambda x: x[1] is not None, matchs))
+    if len(matchs) < 1:
+        raise Exception('Unknow input: %s' % input.split()[0])
+    
+    max_match = max(matchs, key=lambda x: x[1].end())
     return max_match, input[max_match[1].end():].lstrip()
 
 
