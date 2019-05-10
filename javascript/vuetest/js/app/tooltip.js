04cefba14e939ee6dcaf1787beb7dfae0375eda9
haohu <haohu@techaction.cn>
2019-05-10 19:29:09 +0800

    x

--- javascript/vuetest/js/app/tooltip.js
+++ javascript/vuetest/js/app/tooltip.js
@@ -1,7 +1,7 @@
 define(function(require) {
     var $ = require('jquery');
     // 加载 bootstrap 组件
-    require('bootstrap');
+    require('Bootstrap');
     return function() {
         $('[data-toggle="tooltip"]').tooltip();
     }
