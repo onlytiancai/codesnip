83521825e8da9c12d38c61ce08d27d16d5ff097b
onlytiancai <onlytiancai@gmail.com>
2019-07-02 16:04:32 +0800

    x

--- c/study/hash.c
+++ c/study/hash.c
@@ -15,7 +15,6 @@ typedef struct Whash{
 whash *mkhash();
 void freehash(whash *hash);
 
-
 // JS Hash Function  
 static unsigned int JSHash(char *str)  
 {  
@@ -29,15 +28,86 @@ static unsigned int JSHash(char *str)
     return (hash & 0x7FFFFFFF);  
 }
 
-static int hash_put(whash *hash, char *k, char *v) {
-    unsigned int code = JSHash(k);
-    int kindex = code % hash->capacity;
-    printf("put:code=%d, index=%d, k=%s, v=%s\n", code, kindex, k, v);
 
-    // 解决冲突 
-    char *kfound = hash->keys[kindex];
-    if (kfound != NULL) {
-        printf("hash conflict:%d %s %s\n", code, k, kfound); 
+// 返回负数表示找不到，0或正数表示找到
+static int find_empty(whash *hash, char* k) {
+    unsigned int code = JSHash(k);
+    int index, i;
+
+    for (i = 0; i < hash->capacity; i++) {
+        index = code % hash->capacity;
+        if (hash->keys[index] == NULL) {
+            printf("find empty done:count=%d index=%d k=%s\n", i, index, k);
+            return index;
+        }
+        code++;
+    }
+
+    printf("find empty failed:%s\n", k);
+    return -1;
+}
+
+// 返回负数表示找不到，0或正数表示找到
+static int find_key(whash *hash, char* k) {
+    unsigned int code = JSHash(k);
+    int index, i;
+
+    for (i = 0; i < hash->capacity; i++) {
+        index = code % hash->capacity;
+        char *p = hash->keys[index];
+        if (p != NULL && strncmp(k, p, 100) == 0) {
+            printf("find key done:count=%d index=%d k=%s\n", i, index, k);
+            return index;
+        }
+        code++;
+    }
+
+    printf("find key failed:%s\n", k);
+    return -1;
+}
+
+
+// 扩大 hashtable
+static int extend(whash *hash) {
+    // 保存原始数据
+    char **keys = hash->keys;
+    char **values = hash->values;
+    int capacity = hash->capacity;
+
+    // 重新分配内存
+    hash->capacity = hash->capacity * 2;
+    hash->keys = (char**)malloc(hash->capacity * sizeof(char*));
+    memset(hash->keys, 0, hash->capacity * sizeof(char*));
+    hash->values = (char**)malloc(hash->capacity * sizeof(char*));
+    memset(hash->values, 0, hash->capacity * sizeof(char*));
+
+    // rehash
+    int i;
+    printf("========= begin rehash\n");
+    for (i = 0; i < capacity; i++) {
+        hash->put(hash, keys[i], values[i]);
+    }
+    printf("========= end rehash\n");
+
+    return;
+    // free
+    for (i = 0; i < capacity; i++) {
+        free(keys[i]);
+        free(values[i]);
+    }
+    free(keys);
+    free(values);
+
+}
+
+static int hash_put(whash *hash, char *k, char *v) {
+    printf("put: k=%s, v=%s\n", k, v);
+
+    int index = find_empty(hash, k);
+    if (index < 0) {
+        printf("hash full, will extend\n"); 
+        extend(hash);
+        index = find_empty(hash, k);
     }
 
     // 保存 key
@@ -45,25 +115,25 @@ static int hash_put(whash *hash, char *k, char *v) {
     char* kcopy = (char*)malloc(ksize * sizeof(char)+1);
     kcopy[ksize * sizeof(char)] = '\0';
     strncpy(kcopy, k, 100);
-    hash->keys[kindex] = kcopy;
+    hash->keys[index] = kcopy;
 
     // 保存 value
     size_t vsize = strnlen(v, 100);
     char* vcopy = (char*)malloc(vsize * sizeof(char)+1);
     vcopy[vsize * sizeof(char)] = '\0';
     strncpy(vcopy, v, 100);
-    hash->values[kindex] = vcopy;
+    hash->values[index] = vcopy;
 
     return 0;
 }
 
 static char* hash_get(whash *hash, char *k){
-    unsigned int code = JSHash(k);
-    int kindex = code % hash->capacity;
-    printf("get:code=%d, index=%d\n", code, kindex);
-
     // TODO: not found
-    return hash->values[kindex];
+    int index = find_key(hash, k);
+    if (index < 0) {
+        return NULL; 
+    }
+    return hash->values[index];
 }
 
 whash *mkhash() {
@@ -99,11 +169,14 @@ int main() {
         hash->put(hash, keys[i], values[i]);
     }
 
+    printf("========= begin get\n");
+
     for (i = 0; i < size; i++) {
         char *key = keys[i];
         char *value = hash->get(hash, key);
         printf("%s=%s\n", key, value);
     }
+    printf("========= end get\n");
 
     freehash(hash);
     return 0;
