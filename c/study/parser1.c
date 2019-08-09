c818f8ddd87d2ee92ea7ee2d5fb9412556c4d19e
onlytiancai <onlytiancai@gmail.com>
2019-08-09 12:21:52 +0800

    x

--- c/study/parser1.c
+++ c/study/parser1.c
@@ -5,10 +5,10 @@
 #define MAXSIZE 1024
 #define BLOCKSIZE 8 
 
-static int indexof(const char *str, const size_t lenstr, 
-        const char *substr, const size_t lensubstr) {
+// 查找子串的位置
+static int indexof(const char *str, size_t lenstr, const char *substr) {
     int i, j;
-
+    size_t lensubstr = strlen(substr);
     for (i = 0, j = 0; i < lenstr; i++) {
         j = str[i] == substr[j] ? j + 1 : 0;
         if (j == lensubstr) return i - j + 1;
@@ -17,21 +17,22 @@ static int indexof(const char *str, const size_t lenstr,
     return -1;
 }
 
-int main() {
-    char header[MAXSIZE];
-    char *p = header;
-    size_t total = 0;
-    size_t m, n ;
-    char flag[] = "\r\n\r\n"; 
-    size_t lenflag = strlen(flag);
+// 读取到指定位置, fd 是描述子，buffer 是读缓冲区，flag 是结尾标志
+static size_t readto(int fd, char *buffer, const char *flag) {
+    char *p = buffer; // 读缓冲区写入指针
+    size_t total = 0; // 已读取字节数
+    size_t m, n ;     // 每次欲读取字节数及实际读取字节数
+    size_t lenflag;   // flag 长度
 
+    lenflag = strlen(flag);
     m = BLOCKSIZE;
-    while ((n = read(STDIN_FILENO, p, m)) > 0) {
+    while ((n = read(fd, p, m)) > 0) {
         total += n;
-        printf("%d %.*s\n", total, n, p);
+        printf("debug: total=%d n=%d\n", total, n);
 
-        if (indexof(p-lenflag, lenflag + n, flag, lenflag) != -1) {
-            printf("read flag\n%.*s", total, header); 
+        if (indexof(p - lenflag, lenflag + n, flag) != -1) {
+            printf("read flag\n", total);
+            break;
         }
 
         m = MAXSIZE - total > BLOCKSIZE ? BLOCKSIZE : MAXSIZE - total;
@@ -42,5 +43,14 @@ int main() {
 
         p += n;
     }
+
+    return total;
+}
+
+int main() {
+    char buffer[MAXSIZE];           // 读缓冲区
+    int total = readto(STDIN_FILENO, buffer, "\r\n\r\n");
+    printf("result:\n%.*s\n", total, buffer);
+
     return 0;
 }
