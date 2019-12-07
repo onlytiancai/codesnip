f7676a578f46d24ef278cc51d3e87537c2490c28
onlytiancai <onlytiancai@gmail.com>
2019-12-07 18:23:28 +0800

    x

---
+++ python/pingtools/settings.py
@@ -0,0 +1,14 @@
+BASE_DIR='/data/rrddata-ping/'
+addrs = ['139.199.215.179', #'cloud.tencent.com',
+         '106.11.248.38', #  'aliyun.com',
+         '140.143.188.251', #  'ihuhao.com',
+         '220.181.38.148',  #'baidu.com',
+         '151.101.1.69', #'stackoverflow.com',
+          ]
+
+names = ['腾讯云',
+         '阿里云',
+         'ihuhao.com',
+         '百度',
+         'stackoverflow.com',
+          ]
