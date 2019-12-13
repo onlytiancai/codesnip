386f208beea74fe06bea0f3c1c74bf6d857da6be
unknown <onlytiancai@gmail.com>
2019-12-13 16:36:16 +0800

    ntfs reader

---
+++ csharp/ntfsreader-src_v1.0/Everything2/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Everything2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listView1 = new Everything2.ListViewNF();
+            this.name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.path = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.changetime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(850, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 640);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(874, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.name,
+            this.path,
+            this.size,
+            this.changetime});
+            this.listView1.Location = new System.Drawing.Point(12, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(850, 609);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // name
+            // 
+            this.name.Text = "名称";
+            this.name.Width = 150;
+            // 
+            // path
+            // 
+            this.path.Text = "路径";
+            // 
+            // size
+            // 
+            this.size.Text = "大小";
+            // 
+            // changetime
+            // 
+            this.changetime.Text = "修改时间";
+            this.changetime.Width = 120;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 662);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Everythin.net";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ColumnHeader name;
+        private System.Windows.Forms.ColumnHeader path;
+        private System.Windows.Forms.ColumnHeader size;
+        private System.Windows.Forms.ColumnHeader changetime;
+        private ListViewNF listView1;
+    }
+}
+
---
+++ csharp/ntfsreader-src_v1.0/Everything2/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Filesystem.Ntfs;
+using System.Windows.Forms;
+
+namespace Everything2
+{
+    public class ListViewItemComparer : IComparer<ListViewItem>
+    {
+        public int Compare(ListViewItem x, ListViewItem y)
+        {
+            return string.Compare(x.Text, y.Text);                       
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private List<ListViewItem> allItems;
+        private List<ListViewItem> myCache;
+        public Form1()
+        {
+            InitializeComponent();
+
+            allItems = new List<ListViewItem>();
+            myCache = allItems;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.Columns[1].Width = Convert.ToInt32(0.5 * this.listView1.Width);
+            listView1.VirtualMode = true;
+            listView1.RetrieveVirtualItem += ListView1_RetrieveVirtualItem;
+
+            DriveInfo driveToAnalyze = new DriveInfo("c");
+            NtfsReader ntfsReader =
+                new NtfsReader(driveToAnalyze, RetrieveMode.All);
+
+            IEnumerable<INode> nodes =
+                ntfsReader.GetNodes(driveToAnalyze.Name);
+
+            int directoryCount = 0, fileCount = 0;
+
+            foreach (INode node in nodes)
+            {
+                if ((node.Attributes & Attributes.Directory) != 0)
+                    directoryCount++;
+                else
+                    fileCount++;
+
+                ListViewItem lvi = new ListViewItem();
+
+                lvi.Text = node.Name;
+                lvi.SubItems.Add(node.FullName);
+                lvi.SubItems.Add(node.Size.ToString());
+                lvi.SubItems.Add(node.LastChangeTime.ToString());
+
+                allItems.Add(lvi);
+            }
+
+            allItems.Sort((x, y) => string.Compare(x.Text, y.Text));
+            listView1.VirtualListSize = myCache.Count;
+
+            this.toolStripStatusLabel1.Text = string.Format("Directory Count: {0}, File Count {1}", directoryCount, fileCount);
+        }
+
+        private void ListView1_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            if (myCache != null)
+            {
+                e.Item = myCache[e.ItemIndex];
+            }
+            else
+            {
+                //A cache miss, so create a new ListViewItem and pass it back.
+                int x = e.ItemIndex * e.ItemIndex;
+                e.Item = new ListViewItem(x.ToString());
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            DateTime beforeDT = System.DateTime.Now;
+
+            if (textBox1.Text != string.Empty)
+            {
+                int index = allItems.BinarySearch(new ListViewItem(textBox1.Text), new ListViewItemComparer());
+
+
+               
+               
+                if (index >= 0)
+                {
+                    int upper;
+                    for (upper = index; upper < allItems.Count; upper++)
+                    {
+                        if (allItems[upper].Text != textBox1.Text) break;
+                    }
+                    myCache = allItems.GetRange(index, upper - index + 1);
+                }
+                else
+                {
+                    myCache = new List<ListViewItem>();
+                }
+            }
+            else {
+                myCache = allItems;
+            }
+
+            DateTime afterDT = System.DateTime.Now;
+            TimeSpan ts = afterDT.Subtract(beforeDT);
+            listView1.VirtualListSize = myCache.Count;
+            this.toolStripStatusLabel1.Text = string.Format("File Count {0}, Take {1} ms", myCache.Count, ts.TotalMilliseconds);
+            listView1.Invalidate();
+
+        }
+
+    }
+}
