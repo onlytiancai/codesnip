9d357f9d9df1daf15101b80705622cb7f75ce857
unknown <onlytiancai@gmail.com>
2019-12-21 17:01:15 +0800

    x

--- python/textindex.py
+++ python/textindex.py
@@ -13,10 +13,14 @@ t1 = time.time()
 char2map = defaultdict(set)
 char1map = defaultdict(set)
 for word in words:
-    for i in range(len(word)):
-        char1map[word[i]].add(word)
-        if i > 0:
-            char2map[word[i-1:i+1]].add(word)
+    chars = list(set(word))
+    for i in range(len(chars)):
+        char1map[chars[i]].add(word)
+
+for word in words:
+    for i in range(1, len(word)):
+        k = ''.join(word[i-1:i+1])
+        char2map[k].add(word)
                 
 print('build index take time:', (time.time() - t1)*1000)
 print("char1map:",len(char1map))
@@ -27,15 +31,22 @@ s = 'test'
 t1 = time.time()
 set_list = (char1map.get(c, set()) for c in s)
 u = set.intersection(*set_list)
-print('intersection', len(u))
+print('char1 intersection', len(u))
 
 ret = [item for item in u if item.find(s) != -1]
 print('char1 search take time:', (time.time() - t1)*1000, len(ret))
 
 t1 = time.time()
-set_list = (char2map.get(s[i-2:i], set()) for i in range(2,len(s)+1))
+for i in range(len(s)):
+    if i > 0:
+        k = s[i-1:i+1]
+        l = char2map.get(k, set())
+        print(k, len(l))
+set_list = (char2map.get(s[i-1:i+1], set()) for i in range(len(s)) if i > 0)
 u = set.intersection(*set_list)
-print('intersection', len(u))
+print('char2 intersection', len(u))
+
+ret = [item for item in u if item.find(s) != -1]
 print('char2 search take time:', (time.time() - t1)*1000, len(ret))
 
 t1 = time.time()
