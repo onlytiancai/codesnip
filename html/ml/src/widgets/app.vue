cd0050c826581b8fdd3366421d9eaf35cfd8ffd9
Your Name <you@example.com>
2020-01-19 17:04:18 +0800

    x

---
+++ html/ml/src/widgets/app.vue
@@ -0,0 +1,13 @@
+<template>
+<button v-on:click="count++">You clicked me {{ count }} times.</button>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      count: 0
+    };
+  }
+};
+</script>
--- html/ml/src/widgets/datasource.js
+++ html/ml/src/widgets/datasource.js
@@ -1,19 +1,23 @@
 const dataset = require('ml-dataset-iris').getNumbers();
+var Vue = require('vue')
 
-const type =  'datasource';
-const text =  '数据源';
+const type = 'datasource';
+const text = '数据源';
 
+// const App = require('./app.vue')
+import App from './app.vue';
 
 function Node(id, data) {
     this.id = id;
     this.type = type;
     this.text = text;
     this.data = data;
-    
-    $('#'+id).dblclick(this.dblclick.bind(this));
+
+    $('#' + id).dblclick(this.dblclick.bind(this));
 }
 
-Node.prototype.dblclick = function() {
+Node.prototype.dblclick = function () {
+    new Vue({ el: '#app', template: '<App/>', components: { App } })
     console.log('dbclick', this.type, this.text);
 }
 
