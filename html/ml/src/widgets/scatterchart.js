d8307775fb5992666ed15ced917417fb772851bc
Your Name <you@example.com>
2020-01-21 17:09:43 +0800

    xxx

--- html/ml/src/widgets/scatterchart.js
+++ html/ml/src/widgets/scatterchart.js
@@ -1,3 +1,7 @@
+import Vue from "vue";
+import App from './scatterchart.vue';
+const AppClass = Vue.extend(App);
+
 const type = 'scatterchart';
 const text = '散点图';
 
@@ -43,31 +47,17 @@ Node.prototype.dblclick = function () {
     }
 
     const {dataset, fields, classes, classLabels } = ret;
-    var echarts = require('echarts');
-    var option = {
-        xAxis: {},
-        yAxis: {},
-        series: [{
-            data: dataset.map(function (x) {
-                return [x[0], x[3]]
-            }),
-            type: 'scatter',
-            itemStyle: {
-                normal: {
-                    color: function (e) {                        
-                        const c = classes[e.dataIndex];
-                        const colorIndex = classLabels.indexOf(c);
-                        var colorarrays = ["#2F9323","#D9B63A","#2E2AA4","#9F2E61","#4D670C","#BF675F","#1F814A","#357F88","#673509","#310937","#1B9637","#F7393C"];
-                        return colorarrays[colorIndex];
-                    }
-                }
-            },
-        }]
-    };
+    $('#detail-box').html('<div class="chart-tools"></div><div id="main" style="width:100%;height:500px"></div>');
+    this.vue = new AppClass({ data: {
+        dataset: dataset,
+        fields: fields,
+        classes: classes,
+        classLabels: classLabels,
+        xAxis: 0,
+        yAxis: 1,
+    }});
 
-    $('#detail-box').html('<div class="chart-container" style="width:100%;height:500px"></div>');
-    var myChart = echarts.init($('#detail-box .chart-container')[0]);
-    myChart.setOption(option);
+    $('#detail-box .chart-tools').append(this.vue.$mount().$el);
 }
 
    
