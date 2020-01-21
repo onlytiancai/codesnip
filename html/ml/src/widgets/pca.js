4f583423294d7f43975c40a855a615d5fcd93259
Your Name <you@example.com>
2020-01-21 11:05:05 +0800

    pca

--- html/ml/src/widgets/pca.js
+++ html/ml/src/widgets/pca.js
@@ -1,3 +1,5 @@
+const { PCA } = require('ml-pca');
+
 const type =  'pca';
 const text =  '主成分分析';
 
@@ -12,7 +14,44 @@ function Node(id, data) {
 }
 
 Node.prototype.dblclick = function() {
-    console.log('dbclick', this.type, this.text);
+    if (!this.sourceWidget) {
+        $('#detail-box').html(this.type + ':未设置数据源');
+        return;
+    }
+
+    $('#detail-box').html('已设置数据源' + this.sourceWidget.type + this.sourceWidget.id);
+ 
+}
+
+// 获取数据集，数据表组件获取数据用
+Node.prototype.getDataset = function () {
+    console.debug('get dataset:', this.id, this.type);
+
+    if (!this.sourceWidget) {
+        $('#detail-box').html(this.type + ':未设置数据源');
+        return {};
+    }
+
+    const {dataset, fields } = this.sourceWidget.getDataset();
+
+    const pca = new PCA(dataset);
+
+    return {
+        dataset: [pca.getExplainedVariance()],
+        fields: fields,
+    }
+}
+
+// 设置数据源
+Node.prototype.setSource = function (widget) {
+    console.debug('set source', this.id, this.type, widget.id, widget.type);
+    this.sourceWidget = widget;
+}
+
+// 清空数据源
+Node.prototype.clearSource = function () {
+    console.debug('clear source', this.id, this.type);
+    this.sourceWidget = null;
 }
 
 module.exports = {
