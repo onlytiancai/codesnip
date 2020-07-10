ba37f304af9294b3753b52189f2da9c6ccabdee4
haohu <you@example.com>
2020-07-10 17:39:00 +0800

    x

--- javascript/vuechat/client/src/index.js
+++ javascript/vuechat/client/src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import printMe from './print.js';
 import './style.css';
 import Logo from './logo.jpg';
 
@@ -12,8 +13,12 @@ function component() {
 
 	var myIcon = new Image();
 	myIcon.src = Logo;
-
 	element.appendChild(myIcon);
+
+	var btn = document.createElement('button');
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
+	element.appendChild(btn);
 
   return element;
 }
--- javascript/vuechat/client/webpack.config.js
+++ javascript/vuechat/client/webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Output Management'
+		})
+	],
 	output: {
-		filename: 'bundle.js',
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
 	module: {
