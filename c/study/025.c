35cdb9f0e5c6a424272d61c8f3ea44b5cb1572b3
onlytiancai <onlytiancai@gmail.com>
2020-10-27 16:25:01 +0800

    x

--- c/study/025.c
+++ c/study/025.c
@@ -9,23 +9,31 @@ typedef int bool;
 
 bool isMatch(char * s, char * p) {
     bool star = 0;
-    char c;
+    char c, *o_s = s;
     while(c=*p++) {
+        printf("debug:%s %s\n", s, p-1);
         if (star = *p == '*')p++;
         if (!star) {
-            if (*s++ != c && c != '.') return false;
+            s++; 
         } else {
-            if (c != '.') while(*s++ == c) ;
-            else while(*s++ != *p) ;
-            s--;
+            if (c != '.') {
+                while(*s++ == c) ;
+                if (s != o_s) s--;
+                if (s != o_s && *p == c) s--; // aaa a*a 多吃了一个
+                //if (s != o_s && p+1 && *(p+1) == '*') s--; // aa a*c*a 
+            } else {
+                while(*s != '\0' && *s++ != *p) ;
+            }
         }
     }
     return *s == '\0';
 }
 
 int main(int argc, char *argv[]) {
-    printf("actual %d expect %d\n", isMatch("aa", "a"), false); 
+    printf("actual %d expect %d\n", isMatch("aaa", "ab*a*c*a"), true); 
     printf("actual %d expect %d\n", isMatch("aa", "a*"), true); 
+    printf("actual %d expect %d\n", isMatch("aaa", "a*a"), true); 
+    printf("actual %d expect %d\n", isMatch("aa", "a"), false); 
     printf("actual %d expect %d\n", isMatch("ab", ".*"), true); 
     printf("actual %d expect %d\n", isMatch("aab", "c*a*b"), true); 
     printf("actual %d expect %d\n", isMatch("mississippi", "mis*is*p*."), false); 
