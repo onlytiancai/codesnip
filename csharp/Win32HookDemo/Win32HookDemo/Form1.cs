df831b79dd565260c3723be2fe3cef7a58bb0174
Your Name <you@example.com>
2021-01-14 17:24:34 +0800

    x

---
+++ csharp/Win32HookDemo/Win32HookDemo/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Win32HookDemo
+{
+    public partial class Form1 : Form
+    {
+        private delegate void InvokeCallback(string msg);
+        public Form1()
+        {
+            InitializeComponent();
+            KeyboardHook kh = new KeyboardHook(true);
+            kh.KeyDown += Kh_KeyDown;
+
+            MouseHook.Setup();
+            MouseHook.LeftMouseDown += MouseHook_LeftMouseDown;
+        }
+
+        private void MouseHook_LeftMouseDown(int controlId, string title)
+        {
+            if (title.Length > 10) title = title.Substring(0, 10);
+            appendText(string.Format("Left Mouse down:{0} {1}", controlId, title));
+        }
+
+        private void Kh_KeyDown(Keys key, bool Shift, bool Ctrl, bool Alt)
+        {
+
+            appendText(string.Format("Key Down:{0}", key.ToString()));
+        }
+
+        void appendText(string msg) {
+            if (textBox1.InvokeRequired)
+            {
+                InvokeCallback msgCallback = new InvokeCallback(appendText);
+                textBox1.Invoke(msgCallback, new object[] { msg });
+            }
+            else
+            {
+                textBox1.Text = DateTime.Now.ToString()+' ' + msg +"\r\n" + textBox1.Text;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ csharp/Win32HookDemo/Win32HookDemo/Win32.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Drawing;
+
+namespace Win32HookDemo
+{
+    public delegate int HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+    public class NativeMethods
+    {
+         
+
+        [DllImport("user32.dll", EntryPoint = "SendMessage", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        public static extern bool SendMessage(IntPtr hWnd, uint Msg, int wParam, StringBuilder lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr SendMessage(int hWnd, int Msg, int wparam, int lparam);
+
+        const int WM_GETTEXT = 0x000D;
+        const int WM_GETTEXTLENGTH = 0x000E;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+
+ 
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        public static Point GetCursorPosition()
+        {
+            POINT lpPoint;
+            GetCursorPos(out lpPoint);
+            // NOTE: If you need error handling
+            // bool success = GetCursorPos(out lpPoint);
+            // if (!success)
+
+            return lpPoint;
+        }
+
+        public static string GetControlText(IntPtr hWnd)
+        {
+
+            // Get the size of the string required to hold the window title (including trailing null.) 
+            Int32 titleSize = SendMessage((int)hWnd, WM_GETTEXTLENGTH, 0, 0).ToInt32();
+
+            // If titleSize is 0, there is no title so return an empty string (or null)
+            if (titleSize == 0)
+                return String.Empty;
+
+            StringBuilder title = new StringBuilder(titleSize + 1);
+
+            SendMessage(hWnd, (int)WM_GETTEXT, title.Capacity, title);
+
+            return title.ToString();
+        }        
+
+        // https://docs.microsoft.com/zh-cn/windows/win32/api/winuser/nf-winuser-setwindowshookexa?redirectedfrom=MSDN
+        // https://docs.microsoft.com/en-us/windows/win32/winmsg/using-hooks
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int SetWindowsHookEx(HookType idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode, IntPtr wParam, IntPtr lParam);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public class MouseHookStruct
+    {
+        public POINT pt;
+        public int hwnd;
+        public int wHitTestCode;
+        public int dwExtraInfo;
+    }
+
+    internal static class HookCodes
+    {
+        public const int HC_ACTION = 0;
+        public const int HC_GETNEXT = 1;
+        public const int HC_SKIP = 2;
+        public const int HC_NOREMOVE = 3;
+        public const int HC_NOREM = HC_NOREMOVE;
+        public const int HC_SYSMODALON = 4;
+        public const int HC_SYSMODALOFF = 5;
+    }
+
+    public enum HookType
+    {
+        WH_KEYBOARD = 2,
+        WH_MOUSE = 7,
+        WH_KEYBOARD_LL = 13,
+        WH_MOUSE_LL = 14
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public static implicit operator Point(POINT point)
+        {
+            return new Point(point.X, point.Y);
+        }
+        public override string ToString()
+        {
+            return string.Format("x={0},y={1}", X, Y);
+        }
+    }
+
+    /// <summary>
+    ///     The MSLLHOOKSTRUCT structure contains information about a low-level keyboard
+    ///     input event.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MOUSEHOOKSTRUCT
+    {
+        public POINT pt; // The x and y coordinates in screen coordinates
+        public int hwnd; // Handle to the window that'll receive the mouse message
+        public int wHitTestCode;
+        public int dwExtraInfo;
+    }
+
+    /// <summary>
+    ///     The MOUSEHOOKSTRUCT structure contains information about a mouse event passed
+    ///     to a WH_MOUSE hook procedure, MouseProc.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MSLLHOOKSTRUCT
+    {
+        public POINT pt; // The x and y coordinates in screen coordinates. 
+        public int mouseData; // The mouse wheel and button info.
+        public int flags;
+        public int time; // Specifies the time stamp for this message. 
+        public IntPtr dwExtraInfo;
+    }
+
+    internal enum MouseMessage
+    {
+        WM_MOUSEMOVE = 0x0200,
+        WM_LBUTTONDOWN = 0x0201,
+        WM_LBUTTONUP = 0x0202,
+        WM_LBUTTONDBLCLK = 0x0203,
+        WM_RBUTTONDOWN = 0x0204,
+        WM_RBUTTONUP = 0x0205,
+        WM_RBUTTONDBLCLK = 0x0206,
+        WM_MBUTTONDOWN = 0x0207,
+        WM_MBUTTONUP = 0x0208,
+        WM_MBUTTONDBLCLK = 0x0209,
+
+        WM_MOUSEWHEEL = 0x020A,
+        WM_MOUSEHWHEEL = 0x020E,
+
+        WM_NCMOUSEMOVE = 0x00A0,
+        WM_NCLBUTTONDOWN = 0x00A1,
+        WM_NCLBUTTONUP = 0x00A2,
+        WM_NCLBUTTONDBLCLK = 0x00A3,
+        WM_NCRBUTTONDOWN = 0x00A4,
+        WM_NCRBUTTONUP = 0x00A5,
+        WM_NCRBUTTONDBLCLK = 0x00A6,
+        WM_NCMBUTTONDOWN = 0x00A7,
+        WM_NCMBUTTONUP = 0x00A8,
+        WM_NCMBUTTONDBLCLK = 0x00A9
+    }
+
+    /// <summary>
+    ///     The structure contains information about a low-level keyboard input event.
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct KBDLLHOOKSTRUCT
+    {
+        public int vkCode; // Specifies a virtual-key code
+        public int scanCode; // Specifies a hardware scan code for the key
+        public int flags;
+        public int time; // Specifies the time stamp for this message
+        public int dwExtraInfo;
+    }
+
+    internal enum KeyboardMessage
+    {
+        WM_KEYDOWN = 0x0100,
+        WM_KEYUP = 0x0101,
+        WM_SYSKEYDOWN = 0x0104,
+        WM_SYSKEYUP = 0x0105
+    }
+}
