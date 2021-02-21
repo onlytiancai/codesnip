21e4dc3c4fa6a705201a69fe7fe16b48c9640aca
onlytiancai <onlytiancai@gmail.com>
2021-02-21 12:03:09 +0800

    x

--- php/fund-diag/action.php
+++ php/fund-diag/action.php
@@ -17,28 +17,49 @@ class Action
 
     function searchFund()
     {
-        $all_funds = json_decode($this->getAllFund(), true);
-        print_r($all_funds['data'][1]);
+        $all_funds = $this->getAllFund();
+        $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
+        if (mb_strlen($keyword) < 2) return $this->showError('keyword error');
+
+        $result = array_slice(array_filter($all_funds, function($x) use ($keyword){
+            return strpos($x[2], $keyword) !== false;
+        }), 0, 10, false);
+
+        $result = array_map(function($x) {
+            return ['name' => $x[2]];
+        }, $result);
+
+        echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
     }
 
     function getAllFund()
     {
-        return $this->cache->refreshIfExpired("all-fund", function () {
+
+        $data = $this->cache->refreshIfExpired("all-fund", function () {
             $response = $this->http_client->get("{$this->api_url}/v1/fund/all");
             return (string)$response->getBody();
         }, 4*60*60);   
+        $data = json_decode($data, true);
+        return $data['data'];
     }
 
     function run()
     {
         $action = isset($_GET['action']) ? $_GET['action'] : '';
-        if (in_array($action, $this->allow_actions)) {
-            $this->{$action}();
-        } else {
-            http_response_code(404);
-        }
-        exit();
+        if (!in_array($action, $this->allow_actions)) return $this->responseCode(404);
+        $this->{$action}();
     } 
+
+    function responseCode($code)
+    {
+        http_response_code($code);
+        exit();
+    }
+
+    function showError($message)
+    {
+        exit($message);    
+    }
 }
 
 $action = new Action();
--- php/fund-diag/index.php
+++ php/fund-diag/index.php
@@ -4,25 +4,60 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
     <title>Bootstrap 101 Template</title>
-
-    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 shim 和 Respond.js 是为了让 IE8 支持 HTML5 元素和媒体查询（media queries）功能 -->
-    <!-- 警告：通过 file:// 协议（就是直接将 html 页面拖拽到浏览器中）访问页面时 Respond.js 不起作用 -->
     <!--[if lt IE 9]>
       <script src="https://cdn.jsdelivr.net/npm/html5shiv@3.7.3/dist/html5shiv.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/respond.js@1.4.2/dest/respond.min.js"></script>
     <![endif]-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/css/selectize.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"></script>
   </head>
   <body>
-    <h1>你好，世界！</h1>
 
-    <!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"></script>
-    <!-- 加载 Bootstrap 的所有 JavaScript 插件。你也可以根据需要只加载单个插件。 -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+            <div class="control-group">
+                <select id="select-yourself" class="demo-default" placeholder="请输入基金..." multiple="multiple">
+                    <option value="">请输入基金...</option>
+                </select>
+            </div>
+        </div>
+      </div>
+    </div>
+    <script>
+      $(document).ready(function() {
+
+$('#select-yourself').selectize({
+  valueField: 'name',
+  labelField: 'name',
+  searchField: 'name',
+  options: [],
+  create: false,
+  load: function(query, callback) {
+    if (!query.length) return callback();
+    $.ajax({
+      url: 'action.php',
+      type: 'GET',
+      dataType: 'json',
+      data: {
+        action: 'searchFund',
+        keyword: query,
+      },
+      error: function() {
+        callback();
+      },
+      success: function(res) {
+        callback(res);
+      }
+    });
+  }
+});
+        });
+    </script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.6/js/standalone/selectize.min.js"></script>
   </body>
 </html>
