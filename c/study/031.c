711a9b934fa11b0e14b23347433ec0f95d69b870
onlytiancai <onlytiancai@gmail.com>
2021-05-23 21:02:33 +0800

    x

--- c/study/031.c
+++ c/study/031.c
@@ -10,17 +10,11 @@ int main()
         if (c == EOF) break;
         *p++ = c;
 
-        if (s == 0) {
-            s = c == '\r' ? 1: 0;
-        } else if (s == 1) {
-            s = c == '\n' ? 2: 0;
-        } else if (s == 2) {
-            s = c == '\r' ? 3: 0;
-        } else if (s == 3) {
-            s = c == '\n' ? 4: 0;
-        } else {
-            s = 0;
-        }
+        if (s == 0 && c == '\r') s = 1; 
+        else if (s == 1 && c == '\n') s = 2; 
+        else if (s == 2 && c == '\r') s = 3; 
+        else if (s == 3 && c == '\n') s = 4; 
+        else s = 0; 
         
         if (s==4) {
             *(p-4) = '\0';
