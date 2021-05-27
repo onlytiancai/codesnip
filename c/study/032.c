7323a2e7cfbd2ac7353245ed3bb3ef6ed374a330
onlytiancai <onlytiancai@gmail.com>
2021-05-27 16:05:19 +0800

    x

--- c/study/032.c
+++ c/study/032.c
@@ -11,7 +11,7 @@
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 const int N = 10;
 int has_data = 0, use_list = 1, data_len = 0, *g_list, *list1, *list2;
-int epfd = -1;
+int epfd = -1, efd = -1;
 
 void handle_error(char* file, int line) {
     printf("has error: file=\"%s\" line=%d error=\"%s\"\n", file, line, strerror(errno));
@@ -19,9 +19,8 @@ void handle_error(char* file, int line) {
 }
 
 void* thread1(void *data)  {
-    int i, n, *p, ret, efd;
-    struct epoll_event event;
-
+    int i, n, *p, ret;
+    uint64_t count = 1;
     while(1) {
         pthread_mutex_lock(&lock); 
         p = g_list;
@@ -30,17 +29,10 @@ void* thread1(void *data)  {
         n = rand() % N + 1;
         for (i = 0; i < n; ++i) { p[i] = rand(); }
 
-        efd = eventfd(1, EFD_CLOEXEC|EFD_NONBLOCK);
-        if (efd == -1) handle_error(__FILE__, __LINE__);
-        event.data.fd = efd;
-        event.events = EPOLLIN | EPOLLET;
-        ret = epoll_ctl(epfd, EPOLL_CTL_ADD, efd, &event);
-        if (ret != 0) handle_error(__FILE__, __LINE__);
-
         pthread_mutex_lock(&lock); 
         has_data = 1; data_len = n;
 
-        ret = write(efd, (void*)0xffffffff, sizeof(uint64_t));
+        ret = write(efd, &count, sizeof(uint64_t));
         pthread_mutex_unlock(&lock);  
         sleep(1);
     }
@@ -57,7 +49,6 @@ void* thread2(void *data) {
         for (i = 0; i < nfds; i++) {
             if (events[i].events & EPOLLIN) {
                 read(events[i].data.fd, &result, sizeof(uint64_t));
-                close(events[i].data.fd);
 
                 pthread_mutex_lock(&lock); 
                 p = g_list; n = data_len; data_len = 0; has_data = 0;
@@ -88,9 +79,18 @@ int main()
     epfd = epoll_create1(EPOLL_CLOEXEC);
     if (epfd == -1) handle_error(__FILE__, __LINE__);
 
+    efd = eventfd(0, EFD_NONBLOCK);
+    if (efd == -1) handle_error(__FILE__, __LINE__);
+
+    int ret;
+    struct epoll_event event;
+    event.data.fd = efd;
+    event.events = EPOLLIN | EPOLLET;
+    ret = epoll_ctl(epfd, EPOLL_CTL_ADD, efd, &event);
+    if (ret != 0) handle_error(__FILE__, __LINE__);
+
     list1 = l1; list2 = l2;
     g_list = list1; use_list = 1;
-
 
     pthread_mutex_init(&lock,NULL);
     pthread_create(&ptid1, NULL, thread1, "thread1:increase");
