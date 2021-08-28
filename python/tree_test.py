083b6b03a9e5898a277a9dd3b2d4d71c107418fb
onlytiancai <onlytiancai@gmail.com>
2021-08-28 10:26:26 +0800

    x

--- python/tree_test.py
+++ python/tree_test.py
@@ -3,13 +3,17 @@ import textwrap
 from tree import *
 
 class MainTest(unittest.TestCase):
-    def test_depth_first(self):
+    def _read_tree(self):
         txt = textwrap.dedent('''\
         5 3 7
         3 2 4
         7 6 8''')
         tree = read_tree(txt) 
         self.assertTrue(tree is not None)
+        return tree
+
+    def test_depth_first(self):
+        tree = self._read_tree()
         output = depth_first(tree)
         self.assertEqual([5, 3, 2, 4, 7, 6, 8], output)
 
