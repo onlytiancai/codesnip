543d6ece519a22e77b0cc64a4c42bf67427dfdce
Your Name <you@example.com>
2022-05-13 20:13:09 +0800

    d

--- javascript/markdowntodo/index.js
+++ javascript/markdowntodo/index.js
@@ -1,6 +1,23 @@
-var markdown = require( "markdown" ).markdown;
+var md = require( "markdown" ).markdown;
 
 exports.tohtml = function(text)
 {
-    return markdown.toHTML( text );
+    return md.toHTML( text );
 }
+
+const text = `
+### ttt
+
+- todo: aaa
+- doing: bbb
+- done: ccc
+`;
+
+const mdTree = md.parse( text );
+console.log(mdTree);
+
+const htmlTree = md.toHTMLTree(mdTree)
+console.log(htmlTree);
+
+const html = md.renderJsonML(htmlTree);
+console.log( html );
