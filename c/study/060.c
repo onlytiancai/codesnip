8183cfec7b1f7a8b384335396210e52bd6e65f05
onlytiancai <onlytiancai@gmail.com>
2022-08-20 11:36:12 +0800

    x

--- c/study/060.c
+++ c/study/060.c
@@ -71,39 +71,39 @@ void *memcpy3(void *dst, const void *src, size_t n)
 
 void memcpy4(void *dest, const void *src, size_t count)
 {
-	__m128i *srcPtr = (__m128i *)src;
-	__m128i *destPtr = (__m128i *)dest;
+    __m128i *srcPtr = (__m128i *)src;
+    __m128i *destPtr = (__m128i *)dest;
 
-	unsigned int index = 0;
-	while(count) {
+    unsigned int index = 0;
+    while(count) {
 
-		__m128i x = _mm_load_si128(&srcPtr[index]);
-		_mm_stream_si128(&destPtr[index], x);
+        __m128i x = _mm_load_si128(&srcPtr[index]);
+        _mm_stream_si128(&destPtr[index], x);
 
-		count -= 16;
-		index++;
-	}
+        count -= 16;
+        index++;
+    }
 }
 
 void run_test(void *dest, void *src, size_t count, int iterations, int type)
 {
-	if(type==1) {
-		for(int i = 0; i < iterations; i++) {
-			memcpy(dest, src, count);
-		}
-	} else if (type==2){
-		for(int i = 0; i < iterations; i++) {
-			memcpy2(dest, src, count);
-		}
-	} else if (type==3){
-		for(int i = 0; i < iterations; i++) {
-			memcpy3(dest, src, count);
-		}
-	} else if (type==4){
-		for(int i = 0; i < iterations; i++) {
-			memcpy4(dest, src, count);
-		}
-	}
+    if(type==1) {
+        for(int i = 0; i < iterations; i++) {
+            memcpy(dest, src, count);
+        }
+    } else if (type==2){
+        for(int i = 0; i < iterations; i++) {
+            memcpy2(dest, src, count);
+        }
+    } else if (type==3){
+        for(int i = 0; i < iterations; i++) {
+            memcpy3(dest, src, count);
+        }
+    } else if (type==4){
+        for(int i = 0; i < iterations; i++) {
+            memcpy4(dest, src, count);
+        }
+    }
 
 
 }
@@ -111,22 +111,21 @@ void run_test(void *dest, void *src, size_t count, int iterations, int type)
 int main(int argc, char **argv)
 {
     const unsigned int BUFFER_SIZE = 64 * 1024 * 1024;
-	int iterations = 50;
-	unsigned char *buf1 = (unsigned char *)malloc_aligned(BUFFER_SIZE);
-	unsigned char *buf2 = (unsigned char *)malloc_aligned(BUFFER_SIZE);
-	
-	unsigned int t0, t1;
-    for (int i = 1; i <= 4; ++i) {
-        printf("Running memcpy%d\n", i);
-        t0 = getTime();
-        run_test(buf1, buf2, BUFFER_SIZE, iterations, i);
-        t1 = getTime() - t0;
+    int iterations = 50;
+    unsigned char *buf1 = (unsigned char *)malloc_aligned(BUFFER_SIZE);
+    unsigned char *buf2 = (unsigned char *)malloc_aligned(BUFFER_SIZE);
 
-        printf("%.3fs\n", (float)t1/1000);
-    }
+    unsigned int t0, t1;
+    int i = atoi(argv[1]);
+    printf("Running memcpy%d\n", i);
+    t0 = getTime();
+    run_test(buf1, buf2, BUFFER_SIZE, iterations, i);
+    t1 = getTime() - t0;
 
-	free_aligned(buf1);
-	free_aligned(buf2);
+    printf("%.3fs\n", (float)t1/1000);
 
-	return 0;
+    free_aligned(buf1);
+    free_aligned(buf2);
+
+    return 0;
 }
