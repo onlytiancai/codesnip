345f94ea194a34bf369db262b5b3ed8ec0886ae2
onlytiancai <onlytiancai@gmail.com>
2023-01-07 16:29:35 +0800

    x

--- c/study/061.c
+++ c/study/061.c
@@ -11,14 +11,11 @@ const int THREAD_COUNT = 2;
 int number = 0;
 
 void destroy_sem(sem_t *p_sem) {
-    printf("destroy sem:%p\n", p_sem);
     sem_destroy(p_sem);
 }
 
 void *myfunc1(void *arg) {
     sem_t *p_sem = arg;
-    printf("child thread get sem:%lu, %p\n", pthread_self(), p_sem);
-
     for (int i = 0; i < MAX; i++) {
         sem_wait(p_sem);
         number++;
@@ -32,9 +29,8 @@ void test1() {
     number = 0;
     sem_t sem destroy_sem_scope;
     sem_init(&sem, 0, 1);
-    printf("init sem:%p\n", &sem);
 
-    clock_t start, end;
+    clock_t start;
 
     pthread_t threads[THREAD_COUNT];
     for (int i = 0; i < THREAD_COUNT; i++) {
@@ -45,13 +41,12 @@ void test1() {
     for (int i = 0; i < THREAD_COUNT; i++) {
         pthread_join(threads[i], NULL);
     }
-    printf("test1: number=%d, time cost=%fms\n", number, (double)(clock()-start)/CLOCKS_PER_SEC*1000);
+    printf("test1(sem in the loop): number=%d, time cost=%fms\n", number, (double)(clock()-start)/CLOCKS_PER_SEC*1000);
 
 }
 
 void *myfunc2(void *arg) {
     sem_t *p_sem = arg;
-    printf("child thread get sem:%lu, %p\n", pthread_self(), p_sem);
 
     sem_wait(p_sem);
     for (int i = 0; i < MAX; i++) {
@@ -66,9 +61,8 @@ void test2() {
     number = 0;
     sem_t sem destroy_sem_scope;
     sem_init(&sem, 0, 1);
-    printf("init sem:%p\n", &sem);
 
-    clock_t start, end;
+    clock_t start;
 
     pthread_t threads[THREAD_COUNT];
     for (int i = 0; i < THREAD_COUNT; i++) {
@@ -79,11 +73,80 @@ void test2() {
     for (int i = 0; i < THREAD_COUNT; i++) {
         pthread_join(threads[i], NULL);
     }
-    printf("test1: number=%d, time cost=%fms\n", number, (double)(clock()-start)/CLOCKS_PER_SEC*1000);
+    printf("test2(sem_t out the loop): number=%d, time cost=%fms\n", number, (double)(clock()-start)/CLOCKS_PER_SEC*1000);
+
+}
+
+void *myfunc3(void *arg) {
+    pthread_spinlock_t *p_lock = arg;
+
+    pthread_spin_lock(p_lock);
+    for (int i = 0; i < MAX; i++) {
+        number++;
+    }
+    pthread_spin_unlock(p_lock);
+
+    pthread_exit(&number);
+}
+
+void test3() {
+    number = 0;
+    pthread_spinlock_t lock = 0;
+    pthread_spin_init(&lock, PTHREAD_PROCESS_PRIVATE);
+
+    clock_t start;
+
+    pthread_t threads[THREAD_COUNT];
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        pthread_create(&threads[i], NULL, myfunc3, (void*)&lock);
+    }
+
+    start = clock();
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    pthread_spin_destroy(&lock);
+    printf("test3(pthread_spinlock_t out the loop): number=%d, time cost=%fms\n", number, (double)(clock()-start)/CLOCKS_PER_SEC*1000);
+
+}
+
+void *myfunc4(void *arg) {
+    pthread_mutex_t *p_lock = arg;
+
+    pthread_mutex_lock(p_lock);
+    for (int i = 0; i < MAX; i++) {
+        number++;
+    }
+    pthread_mutex_unlock(p_lock);
+
+    pthread_exit(&number);
+}
+
+void test4() {
+    number = 0;
+
+    pthread_mutex_t lock;
+    pthread_mutex_init(&lock, NULL);
+
+    clock_t start;
+
+    pthread_t threads[THREAD_COUNT];
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        pthread_create(&threads[i], NULL, myfunc4, (void*)&lock);
+    }
+
+    start = clock();
+    for (int i = 0; i < THREAD_COUNT; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    pthread_mutex_destroy(&lock);
+    printf("test4(pthread_mutex_t out the loop): number=%d, time cost=%fms\n", number, (double)(clock()-start)/CLOCKS_PER_SEC*1000);
 
 }
 int main(void) {
     test1();
     test2();
+    test3();
+    test4();
     return 0;
 }
