7909739394a9d832a8328b9c312e606e6e1d5531
onlytiancai <onlytiancai@gmail.com>
2023-01-13 14:53:18 +0800

    x

--- javascript/listparser/sum.js
+++ javascript/listparser/sum.js
@@ -1,4 +1,23 @@
-function sum(a, b) {
-    return a + b;
-  }
-  module.exports = sum;
+function parse(text) {
+    if (!text) return { tags: [], data: [] }
+    var tags = new Set(), data = []
+    var lines = text.split('\n').map(x => x.trim()).filter(x => x.length > 0)
+    for (const line of lines) {
+        const reg = /\[(\w+)\]/g
+        let reResult = null, lineTags = new Set();
+        while ((reResult = reg.exec(line)) !== null) {
+            const tag = reResult[1];
+            tags.add(tag)
+            lineTags.add(tag)
+        }                
+        data.push({
+            tags: Array.from(lineTags),
+            text: line.replace(reg, '')
+        })
+    }
+    return {
+        tags: Array.from(tags),
+        data: data
+    }
+}
+module.exports = parse;
--- javascript/listparser/sum.test.js
+++ javascript/listparser/sum.test.js
@@ -1,5 +1,46 @@
-const sum = require('./sum');
+const parse = require('./sum');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('empty', () => {
+    expect(parse('')).toEqual({
+        tags: [],
+        data: []
+    });
+});
+
+test('default', () => {
+    expect(parse(`
+    111
+    222
+    `)).toEqual({
+        tags: [],
+        data: [
+            {
+                tags: [],
+                text: '111',
+            },
+            {
+                tags: [],
+                text: '222'
+            }
+        ]
+    });
+});
+
+test('tags', () => {
+    expect(parse(`
+    [a]111
+    [b][c]222
+    `)).toEqual({
+        tags: ['a', 'b', 'c'],
+        data: [
+            {
+                tags: ['a'],
+                text: '111',
+            },
+            {
+                tags: ['b','c'],
+                text: '222'
+            }
+        ]
+    });
 });
