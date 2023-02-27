5ef65a67dafd2d74225247aaa8da7a858ef911d3
onlytiancai <onlytiancai@gmail.com>
2023-02-27 15:54:34 +0800

    x

--- javascript/calc_prime.js
+++ javascript/calc_prime.js
@@ -1,14 +1,13 @@
+function check(i){
+  for (var j = 2; j < i; j++) {
+    if (i % j == 0) return true;
+  }
+  return false;
+
+}
 function run(n) {
   for (var i = 2; i <= n; i++) {
-    found = false;
-    for (var j = 2; j < i; j++) {
-      if (i % j == 0) {
-        found = true;
-        break;
-      }
-    }
-    if (!found) console.log(i);
+    if (!check(i)) console.log(i);
   }
 }
-
-run(100)
+run(30)
