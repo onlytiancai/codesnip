585ca75a1d634bce82caec5ec3845c114dd075a5
onlytiancai <onlytiancai@gmail.com>
2023-03-23 18:37:09 +0800

    x

--- python/aiohttp/test.py
+++ python/aiohttp/test.py
@@ -5,16 +5,34 @@
 from aiohttp import web
 import aiohttp
 import asyncio
+import aiosqlite
+
+db_name = 'test.db'
 
 async def handle(request):
     async with aiohttp.ClientSession() as session:
         async with session.get('http://127.0.0.1') as response:
             html = await response.text()
-            name = request.match_info.get('name', "Anonymous")
-            text = "Hello, " + name + '\n'
-            return web.Response(text=text)
+
+    async with aiosqlite.connect(db_name) as db:
+        db.row_factory = aiosqlite.Row
+        async with db.execute("SELECT count(*) count FROM mytable") as cursor:
+            row = await cursor.fetchone()
+
+    name = request.match_info.get('name', "Anonymous")
+    row_count = row['count']
+    html_len = len(html)
+    text = f'Hello, {name}, {row_count}, {html_len}\n'
+    return web.Response(text=text)
+
+async def init_db():
+    async with aiosqlite.connect(db_name) as db:
+        await db.execute("CREATE TABLE IF NOT EXISTS mytable(id)")
+        await db.execute("insert into mytable(id) values(?)", '1')
+        await db.commit()
 
 async def my_web_app():
+    await init_db()
     app = web.Application()
     app.add_routes([web.get('/', handle),
                     web.get('/{name}', handle)])
