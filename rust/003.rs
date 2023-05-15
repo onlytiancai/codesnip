e145d5ff653e9c4b2921eb08985f26fa7b2c98a1
onlytiancai <onlytiancai@gmail.com>
2023-05-15 17:32:40 +0800

    x

--- rust/003.rs
+++ rust/003.rs
@@ -20,11 +20,24 @@ struct Point2D {
     y: f64,
 }
 
+
 // Similarly, implement `Display` for `Point2D`.
 impl fmt::Display for Point2D {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         // Customize so only `x` and `y` are denoted.
         write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64, 
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} + {}i", self.real, self.imag)    
     }
 }
 
@@ -51,4 +64,9 @@ fn main() {
     // Error. Both `Debug` and `Display` were implemented, but `{:b}`
     // requires `fmt::Binary` to be implemented. This will not work.
     // println!("What does Point2D look like in binary: {:b}?", point);
+
+    let complex = Complex {real: 3.3, imag: 7.2};
+    println!("Compare complex:");
+    println!("Display: {}", complex);
+    println!("Debug: {:?}", complex);
 }
---
+++ rust/005.rs
@@ -0,0 +1,59 @@
+use std::fmt::{self, Formatter, Display};
+
+struct City {
+    name: &'static str,
+    // 纬度
+    lat: f32,
+    // 经度
+    lon: f32,
+}
+
+impl Display for City {
+    // `f` 是一个缓冲区（buffer），此方法必须将格式化后的字符串写入其中
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        // `write!` 和 `format!` 类似，但它会将格式化后的字符串写入
+        // 一个缓冲区（即第一个参数f）中。
+        write!(f, "{}: {:.3}°{} {:.3}°{}",
+               self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c)
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl Display for Color {
+    
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "RGB ({} {} {}) 0x{:02X}{:02X}{:02X}", 
+               self.red, self.green, self.blue,
+               self.red, self.green, self.blue
+               )
+        
+    }
+}
+
+fn main() {
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ].iter() {
+        println!("{}", *city);
+    }
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ].iter() {
+        // 在添加了针对 fmt::Display 的实现后，请改用 {} 检验效果。
+        println!("{}", *color)
+    }
+}
