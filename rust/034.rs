927c77a842a9f6f35d062f5d34f9a0462d319896
onlytiancai <onlytiancai@gmail.com>
2023-05-25 11:31:53 +0800

    x

--- rust/034.rs
+++ rust/034.rs
@@ -8,6 +8,29 @@ fn unused_function() {}
 fn noisy_unused_function() {}
 // 改正 ^ 增加一个属性来消除警告
 
-fn main() {
-    used_function();
+// 这个函数仅当目标系统是 Linux 的时候才会编译
+#[cfg(target_os = "linux")]
+fn are_you_on_linux() {
+    println!("You are running linux!")
 }
+
+// 而这个函数仅当目标系统 **不是** Linux 时才会编译
+#[cfg(not(target_os = "linux"))]
+fn are_you_on_linux() {
+    println!("You are *not* running linux!")
+}
+
+fn main() {
+
+    used_function();
+
+    are_you_on_linux();
+
+    println!("Are you sure?");
+    if cfg!(target_os = "linux") {
+        println!("Yes. It's definitely linux!");
+    } else {
+        println!("Yes. It's definitely *not* linux!");
+    }
+}
+
---
+++ rust/037.rs
@@ -0,0 +1,50 @@
+use std::fmt::Debug;
+
+trait PrintInOption {
+    fn print_in_option(self);
+}
+
+// 这里需要一个 `where` 从句，否则就要表达成 `T: Debug`（这样意思就变了），
+// 或者改用另一种间接的方法。
+impl<T> PrintInOption for T where
+    Option<T>: Debug {
+    // 我们要将 `Option<T>: Debug` 作为约束，因为那是要打印的内容。
+    // 否则我们会给出错误的约束。
+    fn print_in_option(self) {
+        println!("{:?}", Some(self));
+    }
+}
+
+struct Years(i64);
+
+struct Days(i64);
+
+impl Years {
+    pub fn to_days(&self) -> Days {
+        Days(self.0 * 365)
+    }
+}
+
+
+impl Days {
+    /// 舍去不满一年的部分
+    pub fn to_years(&self) -> Years {
+        Years(self.0 / 365)
+    }
+}
+
+fn old_enough(age: &Years) -> bool {
+    age.0 >= 18
+}
+
+fn main() {
+    let vec = vec![1, 2, 3];
+
+    vec.print_in_option();
+    let age = Years(5);
+    let age_days = age.to_days();
+
+    println!("Old enough {}", old_enough(&age));
+    println!("Old enough {}", old_enough(&age_days.to_years()));
+    // println!("Old enough {}", old_enough(&age_days));
+}
