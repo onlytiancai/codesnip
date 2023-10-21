e98b76083fc06c252b1b516cc02e442ff8b49d4b
onlytiancai <onlytiancai@gmail.com>
2023-10-21 08:57:50 +0800

    x

--- python/parse_log.py
+++ python/parse_log.py
@@ -41,13 +41,48 @@ def _getTokens(rule):
 
     return tokens
 
-def query(log:Iterable[str], rule:str, filter:dict):
+def _select(select, data):
+    result = ''
+    for item in select:
+        if item == '*':
+            for k in sorted(data.keys()):
+                result += data[k] + ' '
+        elif (item in data):
+            result += data[item] + ' '
+    return result.rstrip()
+
+def _group_select(select, buffer):
+    print('debug', select, buffer)
+    result = ''
+    for item in select:
+        if item == 'count(*)':
+            result += str(len(buffer)) + ' '
+        elif (item in buffer[0]):
+            result += buffer[0][item] + ' '
+    return result.rstrip()
+
+def query(log:Iterable[str], rule:str, select:list=['*'], filter:dict={}, group:str=''):
     result = []
+    group_buffer = [] 
+    last_group = None
     for line in log:
         data = parse(rule, line)
         cond = [data[k] == v for k, v in filter.items()]
         if all(cond):
-            result.append(line)
+            if group:
+                current_group = data[group] 
+                if last_group != current_group:
+                    if group_buffer:
+                        result.append(_group_select(select, group_buffer))
+                    group_buffer = []
+                    last_group = current_group
+                group_buffer.append(data)
+            else:
+                selected = _select(select, data)
+                result.append(selected)
+
+    if group_buffer:
+        result.append(_group_select(select, group_buffer))
 
     return result 
 
@@ -55,16 +90,25 @@ import unittest
 class StatTest(unittest.TestCase):
     log = '''10:11 111 222
 10:12 333 444
-10:13 555 666
-10:14 333 666'''
+10:14 333 666
+10:13 555 666'''
     rule = 'a b c'
     def test_filter(self):
-        actual = query(self.log.splitlines(), self.rule, {'b': '333'}) 
+        actual = query(self.log.splitlines(), self.rule, ['*'], {'b': '333'}) 
         expected = ['10:12 333 444', '10:14 333 666']
         self.assertListEqual(actual, expected)
 
-        actual = query(self.log.splitlines(), self.rule, {'b': '333', 'c': '444'}) 
+        actual = query(self.log.splitlines(), self.rule, ['*'], {'b': '333', 'c': '444'}) 
         expected = ['10:12 333 444']
+        self.assertListEqual(actual, expected)
+
+    def test_group_count(self):
+        actual = query(log=self.log.splitlines(), 
+                       rule=self.rule, 
+                       select=['b','count(*)'],
+                       group='b'
+                       )
+        expected = ['111 1', '333 2', '555 1']
         self.assertListEqual(actual, expected)
 
 class ParseTest(unittest.TestCase):
