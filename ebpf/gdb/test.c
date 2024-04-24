30b11e60c9757e479103481c950059debc2b6d5d
root <root@localhost.localdomain>
2024-04-24 14:31:50 +0800

    x

--- ebpf/gdb/test.c
+++ ebpf/gdb/test.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+#include <unistd.h>
 
 struct BBB { int x; int y;};
-typedef struct BBB BBB_t;
-struct AAA { int a;char *s; BBB_t b; char c[6];};
+struct AAA { int a; char *s; struct BBB b;char c[6];};
+void print_data(struct AAA *a) {
+    printf("%s %s %d %d %d\n",
+        a->s, a->c,
+        a->a, a->b.x, a->b.y
+    );
 
-void print_data(char *s, struct AAA *a) { 
-    printf("%s %s %d %d\n", s, a->s, a->b.x,a->b.y);
 }
 
 int main(int argc, char *argv[]) {
-    char *s = "hello";
-    struct AAA a = {.a=888, .s=s, {.x=1024, .y=2048}, .c="world"};
-    print_data(s, &a);
+    struct AAA a = {1024, "hello", {2048, 4096}, "world"};
+    print_data(&a);
+    sleep(1);
+    print_data(&a);
     return 0;
 }
