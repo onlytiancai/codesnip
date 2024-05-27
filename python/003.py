9f23a4a5e76da1e1835b1f0abc1f9ddaf85278b5
onlytiancai <onlytiancai@gmail.com>
2024-05-27 17:15:40 +0800

    x

--- python/003.py
+++ python/003.py
@@ -5,23 +5,90 @@ Token = namedtuple('Token', ['type', 'value'])
 ASTNode = namedtuple('ASTNode', ['type', 'value', 'children'])
 
 def parse(str: str) -> List[Token]:
-    ret, curr = [], ''
+    tokens, curr = [], ''
     for ch in str:
         if '0' <= ch <='9':
             curr += ch
         else:
             if curr:
-                ret.append(Token('N', int(curr)))
+                tokens.append(Token('N', int(curr)))
                 curr = ''
             if ch in ('+', '-', '*', '/'):
-                ret.append(Token(ch, ch))
+                tokens.append(Token(ch, ch))
             else:
                 raise Exception(f'unexpect token:{ch}')
-    print('parse:', ret)
-    return ret
+    if curr:
+        tokens.append(Token('N', int(curr)))
+        curr = ''
+
+    print('parse result:')
+    for token in tokens:
+        print(token)
+
+    return tokens 
 
 def analyze(tokens: List[Token]) -> ASTNode:
-    pass
+    '''
+    add -> mul (+ mul)* | mul (- mul)*
+    mul -> pri (* pri)* | pri (/ pri)*
+    pri -> Num
+    '''
+    global curr
+    curr, tokens_count = 0, len(tokens)
+
+    def peek():
+        return tokens[curr] if curr < tokens_count else Token('', '') 
+
+    def read():
+        global curr
+        ret = tokens[curr] 
+        curr += 1
+        return ret
+
+    def pri():
+        token = read()
+        if token.type != 'N':
+            raise Exception(f'expect numbers: {curr}:{token}')
+        return ASTNode(token.type, token.value, [])
+
+    def mul():
+        child1 = pri();
+        node = child1
+        if child1:
+            while True: 
+                token = peek()
+                if token.value in ['*', '/']:
+                    read()
+                    child2 = pri()
+                    node = ASTNode(token.type, token.value, [child1, child2])
+                    child1 = node
+                else:
+                    break
+        return node
+
+    def add():
+        child1 = mul();
+        node = child1
+        if child1:
+            while True: 
+                token = peek()
+                if token.value in ['+', '-']:
+                    read()
+                    child2 = mul()
+                    node = ASTNode(token.type, token.value, [child1, child2])
+                    child1 = node
+                else:
+                    break
+        return node
+    ret = add()
+
+    print('analyze result:')
+    def print_tree(node, level=0):
+        print(level*'\t' + str(node.value))
+        for child in node.children:
+            print_tree(child, level+1)
+    print_tree(ret)
+    return ret
 
 def evaluate(ast: ASTNode) -> float:
     pass
