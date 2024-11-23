c7871622e6d8c45c79894811a4770e47c81ccf9f
onlytiancai <onlytiancai@gmail.com>
2024-11-23 17:39:51 +0800

    notifycation

--- python/sendmail_v2.py
+++ python/sendmail_v2.py
@@ -1,6 +1,3 @@
-'''
-https://wx.mail.qq.com/list/readtemplate?name=app_intro.html#/agreement/authorizationCode
-'''
 import smtplib
 from email.mime.text import MIMEText
 from configparser import ConfigParser
@@ -10,9 +7,11 @@ conf.read('config.ini')
 msg = MIMEText('Have a nice day guy!')
 msg['Subject'] = 'Hi, Nice to meet you!'
 msg['From'] = 'wawasoft@qq.com'
-msg['To'] = 'onlytiancai@gmail.com'
+msg['To'] = 'hao@ihuhao.com'
 
 with smtplib.SMTP('smtp.qq.com', 587) as server:
     server.starttls()
     server.login(conf['smtp']['user'], conf['smtp']['pass'])
     server.send_message(msg)
+
+
