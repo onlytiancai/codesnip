80b36d5c90bd63e0bd1931367bee50be357e6872
onlytiancai <onlytiancai@gmail.com>
2024-12-21 13:13:07 +0800

    x

--- python/binary_search_file.py
+++ python/binary_search_file.py
@@ -1,14 +1,18 @@
-import uuid
+import random
 import os, sys
 
-def generate_and_write_uuids(file_path):
-    uuid_list = [str(uuid.uuid4()) for _ in range(1000000)]
-    uuid_list.sort()
-    with open(file_path, 'w') as file:
-        for i, uuid_str in enumerate(uuid_list):
+def generate_sorted_file_with_skips(filename, num_lines, max_skip=1000):
+    current_number = 0
+    with open(filename, 'w', encoding='utf-8') as f:
+        for i in range(1, num_lines + 1):
+            skip = random.randint(1, max_skip)
+            current_number += skip
+            line = str(current_number) + '\n'
+            f.write(line)
+            
+            # 每输出1万行打印一次进度
             if i % 10000 == 0:
-                print('gen file', i)
-            file.write(uuid_str + '\n')
+                print(f"已生成 {i} 行")
 
 
 def binary_search_file(filename, target):
@@ -23,7 +27,7 @@ def binary_search_file(filename, target):
                 print('检测到死循环，退出')
                 break
             mid = (low + high) // 2
-            print(f'search: {i}, l={low}, m={mid},h={high}')
+            #print(f'search: {i}, l={low}, m={mid},h={high}')
 
             f.seek(mid)
             if mid != 0: # 如果不正好在行首，则跳过当前行的一部分，保证从下一行开始
@@ -33,20 +37,23 @@ def binary_search_file(filename, target):
             if not line:
                 low = mid + 1
                 continue
-
-            if line < target:
+            # print('debug:', int(line), int(target))
+            if int(line) < int(target):
                 low = mid + 1
-            elif line > target:
+            elif int(line) > int(target):
                 high = mid - 1
             else:
                 return True
 
     return False
 
-file_path = "sorted_test.txt"
+file_path = "sorted_skipped_numbers.txt"
 if not os.path.exists(file_path):
     print('gen file ...')
-    generate_and_write_uuids(file_path)
+    num_lines = 100000000  # 1亿行
+    max_skip = 1000  # 每次最多跳过1000个数字
+    generate_sorted_file_with_skips(file_path, num_lines, max_skip)
+
 
 target_string = sys.argv[1] 
 exists = binary_search_file(file_path, target_string)
