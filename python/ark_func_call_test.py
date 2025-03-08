cd847a4c1d17005f2726c99192907d4cd6c42352
onlytiancai <onlytiancai@gmail.com>
2025-03-08 15:20:03 +0800

    x

--- python/ark_func_call_test.py
+++ python/ark_func_call_test.py
@@ -1,3 +1,30 @@
+'''
+大模型的 function call 测试:
+提供两个函数
+- 一个是根据姓名获取所在城市
+- 一个是根据城市获取天气。
+
+问 AI 某个人那里的天气怎么样，它会根据需要调用相关的 API，如果需要调用多个API它也会多次调用，最终给出一个准确的答案。
+
+有了这个功能，可以大大扩展大模型的能力，可以让大模型做很多事情。
+
+以下是测试输出
+
+$ python3 ark_func_call_test.py '小明那里的天气怎么样？'
+Bot [1.113 s][Use FC]:  function name=get_city_by_name, args=[{"name": "小明"}], resp=[北京]
+Bot [1.973 s][Use FC]:  function name=get_weather_by_city, args=[{"city": "北京"}], resp=[晴天，气温 15-20 度。]
+Bot [1.200 s][FC Summary]:  小明所在的北京天气是晴天，气温 15 - 20 度。
+
+$ python3 ark_func_call_test.py '小红所在城市的天气如何？'
+Bot [1.271 s][Use FC]:  function name=get_city_by_name, args=[{"name": "小红"}], resp=[深圳]
+Bot [1.276 s][Use FC]:  function name=get_weather_by_city, args=[{"city": "深圳"}], resp=[小雨，气温 5-10 度。]
+Bot [0.941 s][FC Summary]:  小红所在的深圳目前的天气是小雨，气温 5-10 度。
+
+$ python3 ark_func_call_test.py '小王在哪个城市'
+Bot [1.300 s][Use FC]:  function name=get_city_by_name, args=[{"name": "小王"}], resp=[获取数据出错]
+Bot [2.191 s][FC Summary]:  很抱歉，在获取小王所在城市的数据时出现了错误，暂时无法为你提供小王所在的城市信息。你可以更换姓名再次询问或者有其他需求也可以告诉我。
+'''
+
 import config
 import json
 import sys
