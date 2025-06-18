3ab3be015424ac01e9fbbee6565702772831a308
wawa <onlytiancai@gmail.com>
2025-06-18 12:18:03 +0800

    增加导数和2:1两栏布局

---
+++ ppt/ml/layouts/two-columns.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div class="slidev-layout w-full h-full grid grid-cols-[2fr_1fr] " >
+    <div class="p-4" >
+      <slot />
+    </div>
+    <div class="p-4">
+      <slot name="right" />
+    </div>
+  </div>
+</template>
