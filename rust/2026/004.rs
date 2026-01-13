eab9304b4dc5dadd97ac819569e0c0a97ae95bb4
huhao <huhao@huhaodeMac-mini.local>
2026-01-13 10:05:40 +0800

    去掉第二个泛型参数

--- rust/2026/004.rs
+++ rust/2026/004.rs
@@ -5,7 +5,7 @@ fn main(){
     println!("before sort");
     print_arr(&arr);
 
-    sort_arr(&mut arr, |a, b| a > b);
+    sort_arr(&mut arr);
 
     println!("after sort");
     print_arr(&arr);
@@ -19,12 +19,12 @@ fn print_arr<T>(arr: &[T])
     }
 }
 
-fn sort_arr<T,F>(arr: &mut [T], mut cmp: F) 
-    where F: FnMut(&T, &T) -> bool
+fn sort_arr<T>(arr: &mut [T]) 
+    where T: Ord
 {
     for i in 0..arr.len() {
         for j in 0..i {
-            if cmp(&arr[j], &arr[i]) {
+            if arr[j] > arr[i] {
                 arr.swap(i, j)                
             }
         }
