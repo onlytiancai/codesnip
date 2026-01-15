440f2fb09739773db8d1325b19bcbe3b31c53913
huhao <huhao@huhaodeMac-mini.local>
2026-01-15 10:42:08 +0800

    三种hashmap value排序

--- rust/rust2026/src/r002.rs
+++ rust/rust2026/src/r002.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, BinaryHeap};
+use itertools::Itertools;
 
 pub fn main() {
     let str = "When we released Claude Code, we expected developers to use it for coding. They did—and then quickly began using it for almost everything else. This prompted us to build Cowork: a simpler way for anyone—not just developers—to work with Claude in the very same way. Cowork is available today as a research preview for Claude Max subscribers on our macOS app, and we will improve it rapidly from here.";
@@ -30,9 +31,30 @@ pub fn main() {
         words.insert("hello".to_string(), 1);
     }
 
+    println!("=== sort by vec");
     let  mut  items: Vec<(&String, &i32)> = words.iter().collect();
     items.sort_by(|a, b| b.1.cmp(a.1));
     for (key, value) in items.iter().take(5)  {
         println!("{}: {}", key, value);
     }
+
+    println!("=== sort by heap");
+    let mut  heap   = BinaryHeap::new();
+    for (k, v) in &words {
+        heap.push((v, k));
+    }
+
+    for _ in 1..6 {
+        if let Some((v, k)) = heap.pop() {
+            println!("{}:{}", k, v);
+        }
+    }
+
+    println!("=== sort by itertools");
+    words.iter()
+        .collect::<Vec<_>>()
+        .into_iter()
+        .sorted_by(|a, b| b.1.cmp(a.1))
+        .take(5)
+        .for_each(|(k,v)| println!("{}:{}", k, v));
 }
