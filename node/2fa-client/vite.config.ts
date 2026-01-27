db9a9a0960c44dead619e1348aaffee5ccd61d83
onlytiancai <onlytiancai@gmail.com>
2026-01-27 19:53:13 +0800

    add tw4

--- node/2fa-client/vite.config.ts
+++ node/2fa-client/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
 })
