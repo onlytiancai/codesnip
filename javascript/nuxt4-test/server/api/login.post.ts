9d5ae70c4391db4c8a3efe4d10e65ff9016c62c7
huhao <huhao@huhaodeMac-mini.local>
2026-02-10 10:17:01 +0800

    使用 hashed password

--- javascript/nuxt4-test/server/api/login.post.ts
+++ javascript/nuxt4-test/server/api/login.post.ts
@@ -2,8 +2,12 @@ export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   const { username, password } = body
 
+  // const hashedPassword = await hashPassword('123456')
+  // console.log('hashedPassword:', hashedPassword)
+  const hashedPassword = '$scrypt$n=16384,r=8,p=1$gAJArRUrtpKouqXJ6wG5Pw$r+c7hYDClxrTuCyCRyLU3VuvkMc+uwjf+xjhKXKqGusvUzxOxdCGORkk8P+36d8OVsqxWsWJdaLIXm1bgJyiCw';
+
   // 模拟数据库校验
-  if (username === 'admin' && password === '123456') {
+  if (username === 'admin' && await verifyPassword(hashedPassword, password)) {
 
       await setUserSession(event, {
           // User data
